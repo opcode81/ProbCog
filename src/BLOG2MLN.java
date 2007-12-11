cd147af096fc99cac16f3e589d19e32397573894
jain <jain@2a9d9b90-6428-0410-aa80-b0978a1f908e>
2007-12-11 18:17:47 +0000

    made compact formulas (via decision trees) optional
    git-svn-id: svn+ssh://svnradig/probcog/trunk/SRLDB/src@144 2a9d9b90-6428-0410-aa80-b0978a1f908e

--- src/BLOG2MLN.java
+++ src/BLOG2MLN.java
@@ -9,14 +9,26 @@ public class BLOG2MLN {
 	public static void main(String[] args) {
 		try {
 			if(args.length < 2) {
-				System.out.println("\n usage: BLOG2MLN <BLOG file(s)> <XML-BIF file> [-g]\n\n" + 
-						             "        -g   guess signatures from network structure rather than obtaining them from the BLOG file(s)\n");
+				System.out.println("\n usage: BLOG2MLN <BLOG file(s)> <XML-BIF file> [options]\n\n" + 
+						             "        -g   guess signatures from network structure rather than obtaining them from the BLOG file(s)\n" +
+						             "        -cF  write compact formulas\n");
 				return;
 			}			
+			boolean guessSigs = false, compact = false;
+			for(int i = 2; i < args.length; i++) {
+				if(args[i].equals("-g"))
+					guessSigs = true;
+				else if(args[i].equals("-cF"))
+					compact = true;
+				else {
+					System.err.println("unknown option " + args[i]);
+					return;
+				}
+			}
 			BLOGModel b = new BLOGModel(args[0].split(","), args[1]);
-			if(args.length >= 3 && args[2].equals("-g"))
+			if(guessSigs)
 				b.guessSignatures();
-			b.toMLN(System.out);
+			b.toMLN(System.out, compact);
 		}
 		catch(Exception e) {
 			e.printStackTrace();
