68c9c0c8f040e626021b72d1192a867c1eddb569
jain <jain@cs.tum.edu>
2008-04-29 19:08:11 +0000

    added display of ground BN and saving via plugins from BNJ
    git-svn-id: svn+ssh://svnradig/probcog/trunk/SRLDB/src@191 2a9d9b90-6428-0410-aa80-b0978a1f908e

--- src/BLNinfer.java
+++ src/BLNinfer.java
@@ -2,13 +2,17 @@ import java.util.Vector;
 import java.util.regex.Pattern;
 
 import edu.tum.cs.bayesnets.relational.core.BLOGModel;
-import edu.tum.cs.bayesnets.relational.core.BayesianLogicNetwork;
+import edu.tum.cs.bayesnets.relational.core.bln.BayesianLogicNetwork;
+import edu.tum.cs.bayesnets.relational.inference.CSPSampler;
 import edu.tum.cs.bayesnets.relational.inference.GroundBLN;
+import edu.tum.cs.bayesnets.relational.inference.LikelihoodWeighting;
 import edu.tum.cs.tools.Stopwatch;
 
 
 public class BLNinfer {
 
+	enum Algorithm {LikelihoodWeighting, CSP};
+	
 	/**
 	 * @param args
 	 */
@@ -20,6 +24,8 @@ public class BLNinfer {
 			String dbFile = null;
 			String query = null;
 			int maxSteps = 1000;
+			Algorithm algo = Algorithm.LikelihoodWeighting;
+			boolean showBN = false;
 			
 			for(int i = 0; i < args.length; i++) {
 				if(args[i].equals("-b"))
@@ -32,17 +38,22 @@ public class BLNinfer {
 					query = args[++i];
 				else if(args[i].equals("-e"))
 					dbFile = args[++i];				
-				else if(args[i].equals("-e"))
-					dbFile = args[++i];				
+				else if(args[i].equals("-s"))
+					showBN = true;				
 				else if(args[i].equals("-maxSteps"))
 					maxSteps = Integer.parseInt(args[++i]);
+				else if(args[i].equals("-lw"))
+					algo = Algorithm.LikelihoodWeighting;
+				else if(args[i].equals("-csp"))
+					algo = Algorithm.CSP;
 				else
 					System.err.println("Warning: unknown option " + args[i] + " ignored!");
 			}			
 			if(bifFile == null || dbFile == null || blogFile == null || blnFile == null || query == null) {
 				System.out.println("\n usage: inferBLN <-b <BLOG file>> <-x <xml-BIF file>> <-l <BLN file>> <-e <evidence db>> <-q <comma-sep. queries>> [options]\n\n"+
-							         "    -maxSteps  the maximum number of steps to take\n" + 
-							         "    ");
+							         "    -maxSteps #    the maximum number of steps to take\n" + 
+							         "    -lw            algorithm: likelihood weighting\n" +
+							         "    -csp           algorithm: CSP-based sampling");
 				return;
 			}			
 
@@ -66,10 +77,18 @@ public class BLNinfer {
 			// run inference
 			BayesianLogicNetwork bln = new BayesianLogicNetwork(new BLOGModel(blogFile, bifFile), blnFile);
 			GroundBLN gbln = new GroundBLN(bln, dbFile);
+			if(showBN)
+				gbln.getGroundNetwork().show("/usr/wiss/jain/work/code/BNJ/plugins");
 			Stopwatch sw = new Stopwatch();
 			sw.start();
-			gbln.infer(queries.toArray(new String[0]), maxSteps, 100);
-			//gbln.inferAIS(maxSteps);
+			switch(algo) {
+			case LikelihoodWeighting:
+				new LikelihoodWeighting(gbln).infer(queries.toArray(new String[0]), maxSteps, 100);
+				break;
+			case CSP:
+				new CSPSampler(gbln).infer(queries.toArray(new String[0]), maxSteps, 100);
+				break;
+			}				
 			sw.stop();
 			System.out.println("Inference time: " + sw.getElapsedTimeSecs() + " seconds");
 		}
