bd3a652ad054dc1dbf8206939ebb7fef5eb8cd14
jain <jain@2a9d9b90-6428-0410-aa80-b0978a1f908e>
2008-04-29 19:05:51 +0000

    some refactoring proper instantiation of ground BLNs where the order of the domain product and constant parents in the relational model are correctly considered
    git-svn-id: svn+ssh://svnradig/probcog/trunk/SRLDB/src@188 2a9d9b90-6428-0410-aa80-b0978a1f908e

---
+++ src/edu/tum/cs/bayesnets/relational/core/bln/BayesianLogicNetwork.java
@@ -0,0 +1,51 @@
+package edu.tum.cs.bayesnets.relational.core.bln;
+
+import java.util.Iterator;
+import java.util.Properties;
+import java.util.Vector;
+
+import org.python.core.PyObject;
+import org.python.core.PyObject.ConversionException;
+import org.python.util.PythonInterpreter;
+
+import edu.tum.cs.bayesnets.relational.core.RelationalBeliefNetwork;
+import edu.tum.cs.tools.JythonInterpreter;
+
+public class BayesianLogicNetwork {
+	public RelationalBeliefNetwork rbn;
+	public String logicFile;
+	protected JythonInterpreter jython;
+	protected State state;
+	
+	public BayesianLogicNetwork(RelationalBeliefNetwork rbn, String logicFile) {
+		this.rbn = rbn;
+		this.logicFile = logicFile;
+		state = null;
+		
+		// initialize jython interpreter
+		System.out.println("initializing interpreter...");
+		Properties props = new Properties();
+		props.put("python.path", ".:/usr/wiss/jain/work/code/SRLDB/bin:/usr/wiss/jain/work/code/SRLDB/datagen:/usr/wiss/jain/work/code/SRLDB/mln");
+		PythonInterpreter.initialize(System.getProperties(), props, null);
+		jython = new JythonInterpreter();
+		
+		// load MLN 
+		System.out.println("importing libraries...");
+		jython.exec("from MLN import MLN");
+		System.out.println("loading logic network...");
+		jython.exec("mln = MLN('%s')", logicFile);
+	}
+	
+	public void generateGroundFormulas(String domainFile) {
+		jython.exec("mln.combineDB('%s')", domainFile);		
+		state = new State(jython);
+	}
+	
+	public State getState() {
+		return state;
+	}
+	
+	public Iterable<GroundFormula> iterGroundFormulas() throws ConversionException {
+		return new GroundFormulaIteration(this);
+	}
+}
---
+++ src/edu/tum/cs/bayesnets/relational/inference/BackwardSampling.java
@@ -0,0 +1,5 @@
+package edu.tum.cs.bayesnets.relational.inference;
+
+public class BackwardSampling {
+	
+}
