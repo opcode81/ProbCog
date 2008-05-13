d840ede7e1ec262172dcf187b4e7b563e2bbe15a
jain <jain@cs.tum.edu>
2008-05-13 17:14:27 +0000

    new tools
    git-svn-id: svn+ssh://svnradig/probcog/trunk/SRLDB/src@218 2a9d9b90-6428-0410-aa80-b0978a1f908e

---
+++ src/edu/tum/cs/tools/StringTool.java
@@ -0,0 +1,26 @@
+package edu.tum.cs.tools;
+
+import java.util.Collection;
+
+public class StringTool {
+	public static String join(String glue, Object[] elems) {
+		StringBuffer res = new StringBuffer();
+		for(int i = 0; i < elems.length; i++) {
+			res.append(elems[i].toString());
+			if(i < elems.length-1)
+				res.append(glue);
+		}
+		return res.toString();
+	}
+	
+	public static String join(String glue, Collection<?> elems) {
+		StringBuffer res = new StringBuffer();
+		int i = 0; 
+		for(Object elem : elems) {
+			if(i++ > 0)
+				res.append(glue);
+			res.append(elem.toString());
+		}
+		return res.toString();
+	}
+}
