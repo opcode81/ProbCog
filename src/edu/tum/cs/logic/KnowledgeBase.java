2473287e0bb787b4792662c7d07a7c11d2c2888f
jain <jain@cs.tum.edu>
2008-05-27 15:10:09 +0000

    proper KB reading with comments ignored
    git-svn-id: svn+ssh://svnradig/probcog/trunk/SRLDB/src@246 2a9d9b90-6428-0410-aa80-b0978a1f908e

--- src/edu/tum/cs/logic/KnowledgeBase.java
+++ src/edu/tum/cs/logic/KnowledgeBase.java
@@ -3,12 +3,16 @@ package edu.tum.cs.logic;
 import java.io.BufferedReader;
 import java.io.FileReader;
 import java.io.IOException;
+import java.io.StringReader;
 import java.util.Iterator;
 import java.util.Vector;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 import edu.tum.cs.bayesnets.relational.core.Database;
 import edu.tum.cs.logic.parser.FormulaParser;
 import edu.tum.cs.logic.parser.ParseException;
+import edu.tum.cs.tools.FileUtil;
 
 /**
  * class that represents a logical knowledge base
@@ -30,15 +34,26 @@ public class KnowledgeBase implements Iterable<Formula> {
 	 */
 	public KnowledgeBase(String filename) throws IOException, ParseException {
 		this();
-		// TODO do this properly, skipping the comments
-		BufferedReader br = new BufferedReader(new FileReader(new java.io.File(filename)));
+		// read KB file
+		String fileContent = FileUtil.readTextFile(filename);
+		// remove comments
+		Pattern comments = Pattern.compile("//.*$|/\\*.*?\\*/", Pattern.MULTILINE | Pattern.DOTALL);
+		Matcher matcher = comments.matcher(fileContent);
+		fileContent = matcher.replaceAll("");		
+		// read lines
+		BufferedReader br = new BufferedReader(new StringReader(fileContent));
 		String line;
 		for(;;) {
 			line = br.readLine();
 			if(line == null)
 				break;
-			if(line.endsWith(".") && !line.startsWith("/"))
+			line = line.trim();
+			if(line.length() == 0)
+				continue;
+			if(line.endsWith("."))
 				formulas.add(FormulaParser.parse(line.substring(0, line.length()-1)));
+			else
+				System.err.println("Warning: Line without terminating period ignored: " + line);
 		}
 	}
 	
