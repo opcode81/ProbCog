223d56332c88e7020a30354f7394baf2902c43a7
jain <jain@2a9d9b90-6428-0410-aa80-b0978a1f908e>
2008-06-26 16:35:37 +0000

    support learning of models with decision nodes
    git-svn-id: svn+ssh://svnradig/probcog/trunk/SRLDB/src@274 2a9d9b90-6428-0410-aa80-b0978a1f908e

--- src/edu/tum/cs/bayesnets/relational/learning/DomainLearner.java
+++ src/edu/tum/cs/bayesnets/relational/learning/DomainLearner.java
@@ -6,6 +6,8 @@ import java.util.Set;
 import edu.ksu.cis.bnj.ver3.core.BeliefNode;
 import edu.ksu.cis.bnj.ver3.core.Discrete;
 import edu.tum.cs.bayesnets.relational.core.Database;
+import edu.tum.cs.bayesnets.relational.core.DecisionNode;
+import edu.tum.cs.bayesnets.relational.core.ExtendedNode;
 import edu.tum.cs.bayesnets.relational.core.RelationalBeliefNetwork;
 import edu.tum.cs.bayesnets.relational.core.RelationalNode;
 import edu.tum.cs.bayesnets.relational.core.Signature;
@@ -21,14 +23,18 @@ public class DomainLearner extends edu.tum.cs.bayesnets.learning.DomainLearner {
 		RelationalBeliefNetwork bn = (RelationalBeliefNetwork)this.bn;
 		BeliefNode[] nodes = bn.bn.getNodes();
 		for(int i = 0; i < nodes.length; i++) {
-			RelationalNode node = bn.getRelationalNode(i);
-			// for built-in predicates, set standard boolean domain
-			if(node.isBuiltInPred()) {
+			ExtendedNode extNode = bn.getExtendedNode(i);
+			boolean mustApplyBooleanDomain = false;
+			if(extNode instanceof DecisionNode)
+				mustApplyBooleanDomain = true;			
+			// for decision nodes and built-in predicates, set standard boolean domain
+			if(mustApplyBooleanDomain || ((RelationalNode)extNode).isBuiltInPred()) {
 				((HashSet<String>)directDomainData[i]).add("True");
 				((HashSet<String>)directDomainData[i]).add("False");
 				continue;
-			}
+			}			
 			// for regular nodes, get all values from the database
+			RelationalNode node = (RelationalNode)extNode;
 			if(debug) System.out.println("node: " + node);
 			Signature sig = bn.getSignature(node.getFunctionName());
 			if(sig == null) {
@@ -51,9 +57,12 @@ public class DomainLearner extends edu.tum.cs.bayesnets.learning.DomainLearner {
 		for(int i = 0; i < nodes.length; i++) {
 			System.out.println(nodes[i].getName());
 			if(RelationalBeliefNetwork.isBooleanDomain((Discrete)nodes[i].getDomain())) {
-				Signature sig = bn.getSignature(bn.getRelationalNode(i));
-				if(sig != null)
-					sig.returnType = "Boolean";
+				ExtendedNode extNode = bn.getExtendedNode(i);
+				if(extNode instanceof RelationalNode) {
+					Signature sig = bn.getSignature((RelationalNode)extNode);
+					if(sig != null)
+						sig.returnType = "Boolean";
+				}
 				bn.bn.changeBeliefNodeDomain(nodes[i], new Discrete(new String[]{"True", "False"}));
 			}
 		}
