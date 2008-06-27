871fcecbf1f9db066abbe2ee537b459d8087df73
jain <jain@2a9d9b90-6428-0410-aa80-b0978a1f908e>
2008-06-27 18:19:00 +0000

    added error-reporting when required facts are missing from the database
    git-svn-id: svn+ssh://svnradig/probcog/trunk/SRLDB/src@283 2a9d9b90-6428-0410-aa80-b0978a1f908e

--- src/edu/tum/cs/logic/PossibleWorldFromDatabase.java
+++ src/edu/tum/cs/logic/PossibleWorldFromDatabase.java
@@ -2,6 +2,7 @@ package edu.tum.cs.logic;
 
 import edu.tum.cs.bayesnets.relational.core.Database;
 import edu.tum.cs.bayesnets.relational.core.RelationalBeliefNetwork;
+import edu.tum.cs.bayesnets.relational.core.Signature;
 
 public class PossibleWorldFromDatabase implements IPossibleWorld {
 
@@ -17,12 +18,19 @@ public class PossibleWorldFromDatabase implements IPossibleWorld {
 
 	public boolean isTrue(GroundAtom ga) {
 		try {
-			if(rbn.getSignature(ga.predicate).isBoolean()) {
-				return db.getVariableValue(ga.toString(), closedWorld).equalsIgnoreCase("True");
+			Signature sig = rbn.getSignature(ga.predicate);			
+			if(sig.isBoolean()) {
+				String value = db.getVariableValue(ga.toString(), closedWorld);
+				if(value == null)
+					throw new RuntimeException("Value of " + ga + " not in database; however it must always be given because it is used in a precondition/decision node.");
+				return value.equalsIgnoreCase("True");
 			}
 			else {
 				String varName = rbn.gndAtom2VarName(ga);
-				return db.getVariableValue(varName, closedWorld).equals(ga.args[ga.args.length-1]);			
+				String value = db.getVariableValue(varName, closedWorld);
+				if(value == null)
+					throw new RuntimeException("Value of " + varName + " not in database; however it must always be given because it is used in a precondition/decision node.");
+				return value.equals(ga.args[ga.args.length-1]);			
 			}
 		}
 		catch(Exception e) {
