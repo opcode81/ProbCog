c6f2aa4d2ffada32c0a3bf8cd89f0534c46a3438
jain <jain@2a9d9b90-6428-0410-aa80-b0978a1f908e>
2008-07-09 14:04:45 +0000

    added probcog server stuff
    git-svn-id: svn+ssh://svnradig/probcog/trunk/SRLDB/src@314 2a9d9b90-6428-0410-aa80-b0978a1f908e

---
+++ src/edu/tum/cs/probcog/Model.java
@@ -0,0 +1,35 @@
+package edu.tum.cs.probcog;
+
+import java.util.HashMap;
+
+public abstract class Model {
+	private HashMap<String,String> parameters;
+	protected String name;
+	
+	public Model(String name) {
+		parameters = new HashMap<String,String>();
+		this.name = name;
+	}
+	public abstract void setEvidence(Iterable<String[]> evidence) throws Exception;
+	public abstract void instantiate() throws Exception;
+	public abstract java.util.Vector<InferenceResult> infer(Iterable<String> queries) throws Exception;
+	
+	protected String getParameter(String key) {
+		return parameters.get(key);
+	}
+	
+	protected String getParameter(String key, String defaultValue) {
+		String value = parameters.get(key);
+		if(value == null)
+			return defaultValue;
+		return value;
+	}
+	
+	protected Integer getIntParameter(String key, Integer defaultValue) {
+		return Integer.parseInt(getParameter(key, defaultValue.toString()));
+	}
+	
+	protected void setParameter(String key, String value) {
+		parameters.put(key, value);
+	}
+}
