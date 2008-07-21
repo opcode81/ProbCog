71a51940850d28ce3b1b16abde89188d38759616
jain <jain@cs.tum.edu>
2008-07-21 14:20:24 +0000

    added full reading of XML model pool files
    git-svn-id: svn+ssh://svnradig/probcog/trunk/SRLDB/src@332 2a9d9b90-6428-0410-aa80-b0978a1f908e

--- src/edu/tum/cs/probcog/ModelPool.java
+++ src/edu/tum/cs/probcog/ModelPool.java
@@ -1,5 +1,6 @@
 package edu.tum.cs.probcog;
 
+import java.io.File;
 import java.io.FileReader;
 import java.io.IOException;
 import java.util.HashMap;
@@ -14,44 +15,103 @@ import edu.tum.cs.logic.parser.ParseException;
 
 public class ModelPool {
 	protected HashMap<String, Model> pool;
+	protected File poolPath;
 	
 	public ModelPool(String poolFilename) throws IOException, ParseException, Exception {
 		pool = new HashMap<String, Model>();
 		
-		/*
+		File poolFile = new File(poolFilename);
+		poolPath = poolFile.getParentFile();
+		
 		XMLReader xr = XMLReaderFactory.createXMLReader();
 		PoolReader handler = new PoolReader();
 		xr.setContentHandler(handler);
 		xr.setErrorHandler(handler);
-	    FileReader r = new FileReader(poolFilename);
-		xr.parse(new InputSource(r));
-		*/		 
-		
-		Model m = new BLNModel("tableSetting", "/usr/stud/waldhers/kitchen/new/meals_any_for.blog", "/usr/stud/waldhers/kitchen/new/meals_any_for.learnt.xml", "/usr/stud/waldhers/kitchen/new/meals_any_for.bln");
-		pool.put(m.getName(), m);
+	    FileReader r = new FileReader(poolFile);
+		xr.parse(new InputSource(r));			 
 	}
 	
 	public Model getModel(String name) {
 		return pool.get(name);
 	}
 	
-	public class PoolReader extends DefaultHandler {
-		public class Model {
+	protected class PoolReader extends DefaultHandler {
+		protected class ModelData {
+			String name, type, path;
+			HashMap<String, String> files;
+			HashMap<String, String> params;
 			
+			public ModelData() {
+				files = new HashMap<String,String>();
+			}
+			
+			private void checkFileTypes(String[] requiredTypes) throws Exception {
+				for(String t : requiredTypes) {
+					if(!files.containsKey(t))
+						throw new Exception(String.format("Missing file of type '%s' for model '%s'", t, name));
+				}
+			}
+			
+			public Model instantiate() throws Exception {
+				Model m; 
+				if(name == null)
+					throw new Exception("Model has no 'name' attribute.");
+				System.out.println("Loading model " + name + "...");
+				// get model path
+				File fPath;
+				if(path == null)
+					fPath = poolPath;
+				else
+					fPath = new File(path);
+				if(!fPath.isAbsolute())
+					fPath = new File(poolPath, path);
+				// instantiate
+				if(type.equals("BLN")) {
+					checkFileTypes(new String[]{"network", "decls", "logic"});
+					m = new BLNModel(name, new File(fPath, files.get("decls")).getPath(), new File(fPath, files.get("network")).getPath(), new File(fPath, files.get("logic")).getPath());
+				}
+				else if(type.equals("MLN")) {
+					throw new RuntimeException("MLNs not yet supported");
+				}
+				else
+					throw new Exception(String.format("Unknown model type '%s'", type));
+				return m;
+			}
 		}
-		//enum ModelType {BLN, MLN}; 
+
+		ModelData currentModel;
 		
-		Model currentModel;
-		
-		public void startElement(String uri, String name, String qName, Attributes atts) {
+		public void startElement(String uri, String name, String qName, Attributes attrs) {
 			if(qName.equals("model")) {
-				
+				currentModel = new ModelData();
+				for(int i = 0; i < attrs.getLength(); i++) {
+					String attrName = attrs.getQName(i);
+					if(attrName.equals("name")) 
+						currentModel.name = attrs.getValue(i);
+					else if(attrName.equals("type"))
+						currentModel.type = attrs.getValue(i);
+					else if(attrName.equals("path"))
+						currentModel.path = attrs.getValue(i); 
+					else
+						throw new RuntimeException(String.format("Unhandled attribute '%s' of model.", attrName));
+				}
+			}
+			else if(qName.equals("file")) {
+				String type = attrs.getValue("type");
+				String filename = attrs.getValue("name");
+				if(type == null || filename == null)
+					throw new RuntimeException("The 'filename' tag must have 'type' and 'name' attributes.");
+				currentModel.files.put(type, filename);
 			}
 		}
 	
 		public void endElement(String uri, String name, String qName) {
-			if(qName.equals("model")) {
-				
+			try {
+				if(qName.equals("model"))
+					pool.put(currentModel.name, currentModel.instantiate());				
+			}
+			catch (Exception e) {
+				throw new RuntimeException(e.getMessage());					
 			}
 		}
 	}
