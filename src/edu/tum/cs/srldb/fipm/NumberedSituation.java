5e509c41483aa1504fc6a03ffd3b36d5958df220
jain <jain@2a9d9b90-6428-0410-aa80-b0978a1f908e>
2008-10-30 15:25:58 +0000

    updated to conform to new FIPM version
    git-svn-id: svn+ssh://svnradig/probcog/trunk/SRLDB/src@428 2a9d9b90-6428-0410-aa80-b0978a1f908e

--- src/edu/tum/cs/srldb/fipm/NumberedSituation.java
+++ src/edu/tum/cs/srldb/fipm/NumberedSituation.java
@@ -3,6 +3,7 @@ package edu.tum.cs.srldb.fipm;
 import java.sql.Connection;
 import java.sql.SQLException;
 
+import de.tum.in.fipm.base.data.ReaderException;
 import de.tum.in.fipm.base.models.motion.MotionQueryException;
 import edu.tum.cs.srldb.datadict.DDException;
 import fipm.data.db.models.DBMotionReader;
@@ -13,14 +14,14 @@ public class NumberedSituation extends Situation {
 	
 	public NumberedSituation(Connection connGame, Game game,
 			int time, DBMotionReader mr, int number, Player playerInPossession)
-			throws MotionQueryException, SQLException, DDException {
+			throws MotionQueryException, SQLException, DDException, ReaderException {
 		super(connGame, game, time, mr, playerInPossession);
 		this.number = number;
 	}
 
 	public NumberedSituation(Connection connGame, Game game,
 			int time, DBMotionReader mr, int number)
-			throws MotionQueryException, SQLException, DDException {
+			throws MotionQueryException, SQLException, DDException, ReaderException {
 		this(connGame, game, time, mr, number, null);
 	}
 
