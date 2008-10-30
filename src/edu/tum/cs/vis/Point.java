b7f7ea749a67fb16c21c9993ca5eef85e9d0de37
jain <jain@cs.tum.edu>
2008-10-30 12:48:14 +0000

    some new functionality
    git-svn-id: svn+ssh://svnradig/probcog/trunk/SRLDB/src@424 2a9d9b90-6428-0410-aa80-b0978a1f908e

--- src/edu/tum/cs/vis/Point.java
+++ src/edu/tum/cs/vis/Point.java
@@ -9,13 +9,9 @@ public class Point implements Drawable {
 	public int color;
 	
 	public Point(float x, float y, float z, int color, float size) {
-		this(x,y,z);
+		this.v = new Vector3f(x,y,z);
 		this.color = color;
 		this.size = size;
-	}
-	
-	public Point(float x, float y, float z) {
-		this.v = new Vector3f(x,y,z);
 	}
 	
 	public Point(Point p) {
@@ -37,32 +33,11 @@ public class Point implements Drawable {
 		return String.format("(%f/%f/%f)", v.x, v.y, v.z);
 	}
 
-	/*
-
-	
-
-	
-	public double length() {
-		return Math.sqrt(x*x+y*y+z*z);
-	}
-	
-	public double angleTo(Point p) {
-		float dot = x*p.x + y*p.y + z*p.z;
-		return Math.acos(dot / length() / p.length());  
-	}
-	*/
-
-
 	/**
 	 * copies the position of another point into this
 	 * @param p2
 	 */
 	public void copyPos(Point p2) {
-		/*x = p2.x;
-		y = p2.y;
-		z = p2.z;*/
 		v = new Vector3f(p2.v);
-	}
-	
-	
+	}	
 }
--- src/edu/tum/cs/vis/STIsomap.java
+++ src/edu/tum/cs/vis/STIsomap.java
@@ -6,7 +6,6 @@ import processing.core.PApplet;
 import edu.tum.cs.tools.MySQLConnection;
 
 public class STIsomap extends AnimatedCanvas {
-
 	private static final long serialVersionUID = 1L;
 
 	@Override
@@ -17,20 +16,18 @@ public class STIsomap extends AnimatedCanvas {
 		String password = "UEY9KbNb";
 		
 		// coordinate system
-		add(new CoordinateSystem(500));
+		add(new CoordinateSystem(4000));
 		
 		// 3D trajectory
-		Trajectory traj = new Trajectory();
-		addAnimated(traj);
+		Trajectory traj = new Trajectory();		
 		//add(traj);
 		try {
 			MySQLConnection conn = new MySQLConnection(host, user, password, db);
-			ResultSet rs = conn
-					.select("select x,y,z from STT_DETAILED_ABSTRACT_EXP_ISOMAP3D_INTERVAL where episode_nr=0 and occurrence_nr=1 order by instance_nr");
+			ResultSet rs = conn.select("select x,y,z from STT_DETAILED_ABSTRACT_EXP_ISOMAP3D_INTERVAL where episode_nr=0 and occurrence_nr=1 order by instance_nr");
 			while (rs.next()) {
-				float x = rs.getFloat(1) / 4000 * 500;
-				float y = rs.getFloat(2) / 4000 * 500;
-				float z = rs.getFloat(3) / 4000 * 500;
+				float x = rs.getFloat(1);
+				float y = rs.getFloat(2);
+				float z = rs.getFloat(3);
 				System.out.printf("%f/%f/%f\n", x, y, z);
 				traj.addPoint(x, y, z);
 			}
@@ -40,6 +37,7 @@ public class STIsomap extends AnimatedCanvas {
 		}
 		
 		traj.segment();
+		addAnimated(traj);
 		
 		super.setup();
 	}
