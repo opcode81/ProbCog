f21c89cfc390d74facfa0cda359a07f8841e54e0
jain <jain@2a9d9b90-6428-0410-aa80-b0978a1f908e>
2008-12-04 14:15:55 +0000

    added font selection
    git-svn-id: svn+ssh://svnradig/probcog/trunk/SRLDB/src@490 2a9d9b90-6428-0410-aa80-b0978a1f908e

--- src/edu/tum/cs/vis/items/FrameDisplay.java
+++ src/edu/tum/cs/vis/items/FrameDisplay.java
@@ -1,5 +1,6 @@
 package edu.tum.cs.vis.items;
 
+import processing.core.PFont;
 import edu.tum.cs.vis.AnimatedCanvas;
 import edu.tum.cs.vis.Canvas;
 import edu.tum.cs.vis.Drawable;
@@ -8,15 +9,27 @@ public class FrameDisplay implements Drawable {
 
 	protected AnimatedCanvas canvas;
 	protected int x, y, textAlign;
+	protected PFont font;
 	
-	public FrameDisplay(AnimatedCanvas c, int x, int y, int textAlign) {
+	/**
+	 * 
+	 * @param c
+	 * @param x
+	 * @param y
+	 * @param textAlign
+	 * @param font  may be null if it is not to be explicitly set
+	 */
+	public FrameDisplay(AnimatedCanvas c, int x, int y, int textAlign, PFont font) {
 		this.canvas = c;
 		this.x = x;
 		this.y = y;
 		this.textAlign = textAlign;
+		this.font = font;
 	}
 	
 	public void draw(Canvas c) {
+		if(font != null)
+			c.textFont(font);
 		c.fill(0xffffffff);
 		c.textMode(Canvas.SCREEN);
 		c.textAlign(textAlign);
