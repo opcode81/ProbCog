e61fc30cdd5fcdc553b866d3f0cda1bcd41745d7
jain <jain@2a9d9b90-6428-0410-aa80-b0978a1f908e>
2008-12-10 14:10:22 +0000

    don't display frame number if there is only 1 frame
    git-svn-id: svn+ssh://svnradig/probcog/trunk/SRLDB/src@509 2a9d9b90-6428-0410-aa80-b0978a1f908e

--- src/edu/tum/cs/vis/items/FrameDisplay.java
+++ src/edu/tum/cs/vis/items/FrameDisplay.java
@@ -28,11 +28,14 @@ public class FrameDisplay implements Drawable {
 	}
 	
 	public void draw(Canvas c) {
+		int numSteps = canvas.getMaxAnimationStep()+1;
+		if(numSteps == 1) // don't draw frame display if there is only one frame in the sequence
+			return;
 		if(font != null)
 			c.textFont(font);
 		c.fill(0xffffffff);
 		c.textMode(Canvas.SCREEN);
 		c.textAlign(textAlign);
-		c.text(String.format("%d/%d", canvas.getAnimationStep()+1, canvas.getMaxAnimationStep()+1), x, y);
+		c.text(String.format("%d/%d", canvas.getAnimationStep()+1, numSteps), x, y);
 	}
 }
