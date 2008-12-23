387382b074d74e2924cc987cef5fd5fe6ac7fc98
jain <jain@cs.tum.edu>
2008-12-23 16:49:14 +0000

    added creation of BLNs
    git-svn-id: svn+ssh://svnradig/probcog/trunk/SRLDB/src@536 2a9d9b90-6428-0410-aa80-b0978a1f908e

--- src/PointCloudClassification.java
+++ src/PointCloudClassification.java
@@ -1,16 +1,13 @@
+import java.io.File;
 import java.io.FileNotFoundException;
 import java.io.PrintStream;
+import java.util.Vector;
 
 import edu.tum.cs.analysis.pointcloud.DataReader;
-import edu.tum.cs.bayesnets.core.BeliefNetworkEx;
 import edu.tum.cs.srldb.Database;
-import edu.tum.cs.srldb.IdentifierNamer;
 import edu.tum.cs.srldb.datadict.DDAttribute;
 import edu.tum.cs.srldb.datadict.DDObject;
-import edu.tum.cs.srldb.datadict.IDDRelationArgument;
-
-import java.io.File;
-import java.util.Vector;
+import edu.tum.cs.srldb.datadict.DataDictionary.BLNStructure;
 
 public class PointCloudClassification {
 	
@@ -39,6 +36,7 @@ public class PointCloudClassification {
 		dr.setRename("shadow", "ratioShadow");
 		dr.setRename("angle", "avgNormalAngle");		
 		
+		// read training data from XML files
 		Vector<File> xmls = new Vector<File>();
 		System.out.println("reading XML data...");
 		getXMLFiles(new File("data/training"), xmls);		
@@ -48,12 +46,12 @@ public class PointCloudClassification {
 		}
 		Database db = dr.getDatabase();
 
-		// unused attributes
+		// discard unused attributes
 		String[] unusedAttributes = new String[]{"components", "eig_max", "eig_min", "eig_mid", "avg_z", "avg_d", "flat", "long"};
 		for(String a : unusedAttributes)
 			db.getDataDictionary().getAttribute(a).discard();
 		
-		// clustering
+		// attribute value clustering
 		String[] clusteredAttributes = new String[]{"width", "avgNormalAngle", "length", "height", "sum_low_conf", "ratioShadow", "ratioAbove", "ratioObject", "ratioBelow", "eig_mid", "zero_conf", "avg_z", "avg_d", "eig_max", "eig_min", "little_conf", "thickness"};
 		edu.tum.cs.srldb.datadict.DataDictionary dd = db.getDataDictionary(); 
 		for(String a : clusteredAttributes) {			
@@ -63,45 +61,43 @@ public class PointCloudClassification {
 		}
 		db.doClustering();
 		
-		// finalize
-		db.check();	
 		// set relation properties, i.e. make belongsTo functional
 		dd.getRelation("belongsTo").setFunctional(new boolean[]{false, true});
 
-		// write database
+		// finalize
+		db.check();	
+
+		// write training databases
 		db.writeMLNDatabase(new PrintStream(new File("train.db")));
+		db.writeBLOGDatabase(new PrintStream(new File("train.blogdb")));
 		
-		// basic MLN
+		// MLN
 		PrintStream mln = new PrintStream(new File("pcc.mln"));
 		db.writeBasicMLN(mln);
-		
-		// BLN structure
-		BeliefNetworkEx bn = new BeliefNetworkEx();
-		IdentifierNamer namer = new IdentifierNamer(dd);
-		for(DDAttribute attr : dd.getAttributes()) {
-			String nodeName = String.format("%s(%s)", attr.getName(), namer.getShortIdentifier("object", attr.getOwner().getName()));
-			bn.addNode(nodeName);
-		}
-		for(edu.tum.cs.srldb.datadict.DDRelation rel : dd.getRelations()) {
-			StringBuffer nodeName = new StringBuffer(rel.getName() + "(");
-			IDDRelationArgument[] relargs = rel.getArguments();
-			for(int i = 0; i < relargs.length; i++) {
-				if(i > 0)
-					nodeName.append(',');
-				nodeName.append(namer.getShortIdentifier(rel.getName(), relargs[i].getDomainName()));
-			}
-			nodeName.append(')');
-			bn.addNode(nodeName.toString());
-		}
-		
-		bn.show();
-		
-		// write some formulas
 		DDObject ddo = dd.getObject("object");
-		mln.println("\n// foo formulas");
+		mln.println("\n// formulas");
 		for(DDAttribute dda : ddo.getAttributes().values())
 			if(!dda.isDiscarded())
 				mln.printf("objectT(o,+t) ^ %s(o,+v)\n", dda.getName());		
 		mln.printf("belongsTo(c,o) ^ objectT(o,+ot) ^ componentT(c,+ct)\n");		
+		
+		// BLN structure
+		BLNStructure bs = dd.createBasicBLNStructure();		
+		for(DDAttribute attr : dd.getAttributes()) {
+			if(attr.isDiscarded())
+				continue;
+			if(attr.getName().equals("objectT") || attr.getName().equals("componentT"))
+				continue;
+			String classAttr = attr.getOwner().getName() + "T";			
+			bs.connect(dd.getAttribute(classAttr), attr);
+		}
+		bs.connect(dd.getAttribute("componentT"), dd.getRelation("belongsTo"));
+		bs.connect(dd.getAttribute("objectT"), dd.getRelation("belongsTo"));
+		bs.bn.savePMML("pcc.pmml");
+		//bs.bn.show();
+		
+		// BLN
+		PrintStream bln = new PrintStream(new File("pcc.bln"));
+		dd.writeBasicBLN(bln);
 	}
 }
