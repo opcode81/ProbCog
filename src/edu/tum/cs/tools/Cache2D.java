0590a3445ffada826a66b2771797352038fecedf
jain <jain@2a9d9b90-6428-0410-aa80-b0978a1f908e>
2008-12-23 15:00:48 +0000

    added 2d map and derived 2d cache from it
    git-svn-id: svn+ssh://svnradig/probcog/trunk/SRLDB/src@526 2a9d9b90-6428-0410-aa80-b0978a1f908e

--- src/edu/tum/cs/tools/Cache2D.java
+++ src/edu/tum/cs/tools/Cache2D.java
@@ -1,41 +1,16 @@
-/**
- * 
- */
 package edu.tum.cs.tools;
 
-import java.util.HashMap;
-
-public class Cache2D<TContext, TKey, TValue> {
-	protected HashMap<TContext, HashMap<TKey, TValue>> cache;
-	protected HashMap<TKey, TValue> subcache;
-	protected TKey key;
+public class Cache2D<TContext, TKey, TValue> extends Map2D<TContext, TKey, TValue> {
 	public int numCacheHit = 0;
 	public int numCacheMiss = 0;
 	
-	public Cache2D() {
-		cache = new HashMap<TContext, HashMap<TKey, TValue>>();			
-	}
-	
 	public TValue get(TContext context, TKey key) {
-		this.key = key;
-		subcache = cache.get(context);
-		if(subcache == null) {
-			subcache = new HashMap<TKey, TValue>();
-			cache.put(context, subcache);
-			numCacheMiss++;
-			return null;
-		}
-		
-		TValue value = subcache.get(key);
+		TValue value = this.get(context, key);
 		if(value == null)
 			numCacheMiss++;
 		else 
 			numCacheHit++;
 		return value;
-	}
-	
-	public void put(TValue value) {
-		subcache.put(key, value);
 	}
 	
 	public float getHitRatio() {
