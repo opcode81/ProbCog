75db648a1bf9d32cedd6d09a68b8ff1b70361431
jain <jain@cs.tum.edu>
2009-01-29 14:40:03 +0000

    added proper construction, cleaned up
    git-svn-id: svn+ssh://svnradig/probcog/trunk/SRLDB/src@573 2a9d9b90-6428-0410-aa80-b0978a1f908e

--- src/edu/tum/cs/vis/items/Legend.java
+++ src/edu/tum/cs/vis/items/Legend.java
@@ -11,30 +11,31 @@ import edu.tum.cs.vis.Drawable;
 
 public class Legend implements Drawable {
 	
-	public Vector<String> stringLeg = new Vector<String>();
-	public int[] colors = new int[] {0xffff0000, 0xff00ff00, 0xff0000ff, 0xffffff00, 0xffff00ff, 0xff00ffff, 0xffff8800, 0xffff0088, 0xff88ff00, 0xff00ff88, 0xff8800ff, 0xff0088ff};
-	//public PFont locFont, globFont;
+	int legPosX = 0, legPosY = 0;
 	
-	public void draw(Canvas c) {
-		// TODO Auto-generated method stub
-		//c.textFont(locFont, 11); 
-		int legPosX = 0,legPosY = 0;
-		int i = 0;
-		for (String str : stringLeg){
-			c.fill(colors[i]);
-			c.rect(legPosX + 2, legPosY + 13*(i+2) + 6 , 5,5);
-			c.text(str,legPosX + 10,legPosY + 13*(i+3));
-			i += 1;
-		}
-		//c.textFont(globFont,11);
+	public Legend(java.io.File labelFile, int x, int y) throws NumberFormatException, IOException {
+		legPosX = x;
+		legPosY = y;
+		loadLabels(labelFile);
 	}
 	
-/*	public void loadFonts(Canvas c){
-		locFont = c.loadFont("serif"); 
-		globFont = c.loadFont("Verdana");
-	}*/
+	public Vector<String> stringLeg = new Vector<String>();
+	public static int[] colors = new int[] {0xffff0000, 0xff00ff00, 0xff0000ff, 0xffffff00, 0xffff00ff, 0xff00ffff, 0xffff8800, 0xffff0088, 0xff88ff00, 0xff00ff88, 0xff8800ff, 0xff0088ff};
 	
-	public void loadLabels(java.io.File ascFile) throws NumberFormatException, IOException{
+	public void draw(Canvas c) {
+		int i = 0;
+		for(String str : stringLeg){
+			c.fill(colors[i]);
+			int x = legPosX;
+			int y = legPosY + 13*i;
+			c.rect(x, y-6, 6, 6);
+			c.fill(0xffffffff);
+			c.text(str, x + 10, y);
+			i += 1;
+		}
+	}
+	
+	protected void loadLabels(java.io.File ascFile) throws NumberFormatException, IOException{
 		BufferedReader r = new BufferedReader(new FileReader(ascFile));
 		String line;
 		int iLine = 0;
@@ -43,5 +44,4 @@ public class Legend implements Drawable {
 			iLine++;
 		}
 	}
-
 }
