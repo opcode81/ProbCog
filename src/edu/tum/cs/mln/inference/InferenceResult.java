a631a1f3405fda770e9aab507352a547942ef5e8
jain <jain@2a9d9b90-6428-0410-aa80-b0978a1f908e>
2009-08-05 16:41:30 +0000

    added inference architecture for MLNs
    git-svn-id: svn+ssh://svnradig/probcog/trunk/SRLDB/src@755 2a9d9b90-6428-0410-aa80-b0978a1f908e

---
+++ src/edu/tum/cs/mln/inference/InferenceResult.java
@@ -0,0 +1,17 @@
+package edu.tum.cs.mln.inference;
+
+import edu.tum.cs.logic.GroundAtom;
+
+public class InferenceResult {
+	public GroundAtom ga;
+	public double value;
+	
+	public InferenceResult(GroundAtom ga, double value) {
+		this.value = value;	
+		this.ga = ga;
+	}
+	
+	public void print() {
+		System.out.println(String.format("  %.4f %s", value, ga.toString()));
+	}
+}
