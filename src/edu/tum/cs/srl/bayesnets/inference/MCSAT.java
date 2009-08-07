7bab90dc3457d41b9449bc57ac6cb093ffe383e2
jain <jain@cs.tum.edu>
2009-08-07 17:11:31 +0000

    started MCSAT implementation
    git-svn-id: svn+ssh://svnradig/probcog/trunk/SRLDB/src@776 2a9d9b90-6428-0410-aa80-b0978a1f908e

---
+++ src/edu/tum/cs/srl/bayesnets/inference/MCSAT.java
@@ -0,0 +1,95 @@
+/*
+ * Created on Aug 7, 2009
+ *
+ * TODO To change the template for this generated file go to
+ * Window - Preferences - Java - Code Style - Code Templates
+ */
+package edu.tum.cs.srl.bayesnets.inference;
+
+import java.util.Vector;
+
+import edu.ksu.cis.bnj.ver3.core.BeliefNode;
+import edu.ksu.cis.bnj.ver3.core.CPF;
+import edu.tum.cs.logic.Conjunction;
+import edu.tum.cs.logic.Disjunction;
+import edu.tum.cs.logic.Formula;
+import edu.tum.cs.logic.GroundLiteral;
+import edu.tum.cs.logic.sat.weighted.WeightedClause;
+import edu.tum.cs.srl.bayesnets.bln.GroundBLN;
+
+/**
+ * MC-SAT inference for Bayesian logic networks 
+ * @author jain
+ */
+public class MCSAT extends Sampler {
+
+	protected GroundBLN gbln;
+	protected Vector<WeightedClause> kb;
+	protected double maxWeight = 0;
+	/**
+	 * temporary collection of hard constraints appearing in the CPTs of the ground BN
+	 */
+	protected Vector<Disjunction> hardConstraintsInCPTs = new Vector<Disjunction>();
+	
+	public MCSAT(GroundBLN gbln) throws Exception {
+		this.gbln = gbln;
+		kb = new Vector<WeightedClause>();		
+		// add weighted clauses for probabilistic constraints
+		for(BeliefNode n : gbln.getRegularVariables()) {
+			CPF cpf = n.getCPF();
+			BeliefNode[] domProd = cpf.getDomainProduct();
+			walkCPT4ClauseCollection(cpf, domProd, new int[domProd.length], 0);						
+		}
+		// add weighted clauses for hard constraints
+		double hardWeight = maxWeight + 100;
+		for(Formula f : gbln.getKB()) {
+			f = f.toCNF();
+			if(f instanceof Conjunction) {
+				for(Formula child : ((Conjunction)f).children)
+					kb.add(new WeightedClause(child, hardWeight, true));
+			}
+			else
+				kb.add(new WeightedClause(f, hardWeight, true));
+		}
+		for(Disjunction f : hardConstraintsInCPTs) 
+			kb.add(new WeightedClause(f, hardWeight, true));
+		hardConstraintsInCPTs = null;
+	}
+	
+	protected void walkCPT4ClauseCollection(CPF cpf, BeliefNode[] domProd, int[] domainIndices, int i) throws Exception {
+		if(i == domainIndices.length) {
+			// create disjunction of negated literals corresponding to domain index configuration
+			GroundLiteral[] lits = new GroundLiteral[domainIndices.length];
+			for(int j = 0; j < domainIndices.length; j++) {
+				lits[j] = gbln.getGroundLiteral(domProd[j], domainIndices[j]);
+				lits[j].negate();
+			}
+			Disjunction f = new Disjunction(lits);
+			// obtain probability value and add to collection
+			double p = cpf.getDouble(domainIndices);
+			if(p == 0.0) { // this constraint is actually hard, so remember it for later
+				hardConstraintsInCPTs.add(f);
+			}
+			else { // it is a soft constraint, whose negation we add to the KB
+				double weight = -Math.log(p);
+				kb.add(new WeightedClause(f, weight, false));
+				if(weight > maxWeight)
+					maxWeight = weight;
+			}
+			return;
+		}	
+		// recurse
+		for(int j = 0; j < domProd[i].getDomain().getOrder(); j++) {
+			domainIndices[i] = j;
+			walkCPT4ClauseCollection(cpf, domProd, domainIndices, i+1);
+		}
+	}
+	
+	@Override
+	public Vector<InferenceResult> infer(Iterable<String> queries, int numSamples, int infoInterval) throws Exception {
+		
+		
+		return null;
+	}
+
+}
