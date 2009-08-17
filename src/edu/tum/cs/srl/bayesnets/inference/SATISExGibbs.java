f7cceea58d203db31b5fa7d6ccc2c7530e699963
jain <jain@2a9d9b90-6428-0410-aa80-b0978a1f908e>
2009-08-17 18:11:05 +0000

    added inference algorithm: SAT-IS Extended with interspersed Gibbs sampling
    git-svn-id: svn+ssh://svnradig/probcog/trunk/SRLDB/src@815 2a9d9b90-6428-0410-aa80-b0978a1f908e

---
+++ src/edu/tum/cs/srl/bayesnets/inference/SATISExGibbs.java
@@ -0,0 +1,44 @@
+/*
+ * Created on Aug 17, 2009
+ *
+ * TODO To change the template for this generated file go to
+ * Window - Preferences - Java - Code Style - Code Templates
+ */
+package edu.tum.cs.srl.bayesnets.inference;
+
+import edu.tum.cs.bayesnets.core.BeliefNetworkEx;
+import edu.tum.cs.bayesnets.inference.GibbsSampling;
+import edu.tum.cs.bayesnets.inference.Sampler;
+import edu.tum.cs.bayesnets.inference.WeightedSample;
+import edu.tum.cs.srl.bayesnets.bln.GroundBLN;
+
+public class SATISExGibbs extends SATISEx {
+
+	public SATISExGibbs(GroundBLN bln) throws Exception {
+		super(bln);	
+	}
+	
+	@Override
+	protected Sampler getSampler() {
+		return new SATIS_BSampler_Gibbs(gbln.getGroundNetwork());
+	}	
+	
+	public class SATIS_BSampler_Gibbs extends SATIS_BSampler {
+
+		public GibbsSampling gibbsSampler;
+		
+		public SATIS_BSampler_Gibbs(BeliefNetworkEx bn) {
+			super(bn);
+			gibbsSampler = new GibbsSampling(gbln.getGroundNetwork());
+		}
+		
+		public void onAddedSample(WeightedSample s) {
+			for(int i = 0; i < 9; i++) {
+				gibbsSampler.gibbsStep(this.evidenceDomainIndices, s);
+				addSample(s);
+				currentStep++;
+			}
+		}
+	}
+
+}
