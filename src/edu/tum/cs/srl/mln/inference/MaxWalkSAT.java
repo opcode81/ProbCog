098610f741e7bc92c864a0b6358995afb062f817
jain <jain@cs.tum.edu>
2009-08-20 17:26:44 +0000

    allow different underlying MaxWalkSAT implementations
    git-svn-id: svn+ssh://svnradig/probcog/trunk/SRLDB/src@856 2a9d9b90-6428-0410-aa80-b0978a1f908e

--- src/edu/tum/cs/srl/mln/inference/MaxWalkSAT.java
+++ src/edu/tum/cs/srl/mln/inference/MaxWalkSAT.java
@@ -14,14 +14,19 @@ import edu.tum.cs.logic.sat.weighted.WeightedClausalKB;
 import edu.tum.cs.srl.mln.MarkovRandomField;
 
 public class MaxWalkSAT extends MAPInferenceAlgorithm {
-
+	
 	protected edu.tum.cs.logic.sat.weighted.MaxWalkSAT sat;
 	
 	public MaxWalkSAT(MarkovRandomField mrf) throws Exception {
+		this(mrf, edu.tum.cs.logic.sat.weighted.MaxWalkSAT.class);
+	}
+	
+	public MaxWalkSAT(MarkovRandomField mrf, Class<? extends edu.tum.cs.logic.sat.weighted.MaxWalkSAT> mwsClass) throws Exception {
 		super(mrf);
         WeightedClausalKB wckb = new WeightedClausalKB(mrf, false);
         PossibleWorld state = new PossibleWorld(mrf.getWorldVariables());
-        sat = new edu.tum.cs.logic.sat.weighted.MaxWalkSAT(wckb, state, mrf.getWorldVariables(), mrf.getDb());
+        sat = mwsClass.getConstructor(WeightedClausalKB.class, PossibleWorld.class, edu.tum.cs.logic.WorldVariables.class, edu.tum.cs.srl.Database.class).newInstance(wckb, state, mrf.getWorldVariables(), mrf.getDb());
+        //sat = new edu.tum.cs.logic.sat.weighted.MaxWalkSAT(wckb, state, mrf.getWorldVariables(), mrf.getDb());
 	}
 	
 	@Override
@@ -50,7 +55,7 @@ public class MaxWalkSAT extends MAPInferenceAlgorithm {
 	
 	@Override
 	public String getAlgorithmName() {
-		return super.getAlgorithmName() + "[p=" + sat.getP() + "]";
+		return String.format("MAP/%s[p=%f]", sat.getClass().getSimpleName(), sat.getP());
 	}
 
 }
