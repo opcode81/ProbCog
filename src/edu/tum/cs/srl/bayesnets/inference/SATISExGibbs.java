a7f7922cf463571d0284b5d3163a0b6bfdfb3557
jain <jain@2a9d9b90-6428-0410-aa80-b0978a1f908e>
2009-09-14 16:07:50 +0000

    comments
    git-svn-id: svn+ssh://svnradig/probcog/trunk/SRLDB/src@914 2a9d9b90-6428-0410-aa80-b0978a1f908e

--- src/edu/tum/cs/srl/bayesnets/inference/SATISExGibbs.java
+++ src/edu/tum/cs/srl/bayesnets/inference/SATISExGibbs.java
@@ -12,6 +12,12 @@ import edu.tum.cs.bayesnets.inference.Sampler;
 import edu.tum.cs.bayesnets.inference.WeightedSample;
 import edu.tum.cs.srl.bayesnets.bln.GroundBLN;
 
+/**
+ * SAT-IS Extended (with hard constraints from CPDs) that, for every SAT-IS step, 
+ * adds a number of Gibbs steps to explore the subspace given by the sample;
+ * the samples are weighted the same as the original SAT-IS sample 
+ * @author jain
+ */
 public class SATISExGibbs extends SATISEx {
 
 	public SATISExGibbs(GroundBLN bln) throws Exception {
