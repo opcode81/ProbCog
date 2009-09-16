4165b6f1fb3a9f3958d49f14b0d8693033d354b8
jain <jain@cs.tum.edu>
2009-09-16 18:43:23 +0000

    added comments
    git-svn-id: svn+ssh://svnradig/probcog/trunk/SRLDB/src@944 2a9d9b90-6428-0410-aa80-b0978a1f908e

--- src/edu/tum/cs/tools/Map2D.java
+++ src/edu/tum/cs/tools/Map2D.java
@@ -2,6 +2,10 @@ package edu.tum.cs.tools;
 
 import java.util.HashMap;
 
+/**
+ * a mapping with two-dimensional keys (context/key) 
+ * @author jain
+ */
 public class Map2D<TContext, TKey, TValue> {
 	/**
 	 * the actual two-dimensional mapping
@@ -20,6 +24,12 @@ public class Map2D<TContext, TKey, TValue> {
 		map = new HashMap<TContext, HashMap<TKey, TValue>>();			
 	}
 	
+	/**
+	 * retrieves the value for a particular context/key combination
+	 * @param context
+	 * @param key
+	 * @return
+	 */
 	public TValue get(TContext context, TKey key) {
 		this.key = key;
 		submap = map.get(context);
@@ -31,14 +41,29 @@ public class Map2D<TContext, TKey, TValue> {
 		return submap.get(key);
 	}
 	
+	/**
+	 * retrieves the key-value mapping for a particular context 
+	 * @param context
+	 * @return
+	 */
 	public HashMap<TKey, TValue> getSubmap(TContext context) {
 		return map.get(context);
 	}
 	
+	/**
+	 * stores a new value for the last-retrieved context/key combination
+	 * @param value
+	 */
 	public void put(TValue value) {
 		submap.put(key, value);
 	}
 	
+	/**
+	 * stores the given value under the given context/key combination
+	 * @param context
+	 * @param key
+	 * @param value
+	 */
 	public void put(TContext context, TKey key, TValue value) {
 		submap = map.get(context);
 		if(submap == null) {
