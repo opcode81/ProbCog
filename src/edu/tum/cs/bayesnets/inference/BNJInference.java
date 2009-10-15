e1ddb02dd02a56ef79b2803de01432f6e7df90dc
jain <jain@2a9d9b90-6428-0410-aa80-b0978a1f908e>
2009-10-15 17:16:18 +0000

    added BNJ inference algorithms
    git-svn-id: svn+ssh://svnradig/probcog/trunk/SRLDB/src@994 2a9d9b90-6428-0410-aa80-b0978a1f908e

---
+++ src/edu/tum/cs/bayesnets/inference/BNJInference.java
@@ -0,0 +1,57 @@
+/*
+ * Created on Oct 15, 2009
+ *
+ * TODO To change the template for this generated file go to
+ * Window - Preferences - Java - Code Style - Code Templates
+ */
+package edu.tum.cs.bayesnets.inference;
+
+import edu.ksu.cis.bnj.ver3.core.BeliefNode;
+import edu.ksu.cis.bnj.ver3.core.CPF;
+import edu.ksu.cis.bnj.ver3.core.DiscreteEvidence;
+import edu.tum.cs.bayesnets.core.BeliefNetworkEx;
+
+/**
+ * general wrapper for BNJ inference algorithms
+ * @author jain
+ */
+public class BNJInference extends Sampler {
+
+	Class<? extends edu.ksu.cis.bnj.ver3.inference.Inference> algorithmClass;
+	
+	public BNJInference(BeliefNetworkEx bn, Class<? extends edu.ksu.cis.bnj.ver3.inference.Inference> algoClass) {
+		super(bn);
+		this.algorithmClass = algoClass;
+	}
+
+	@Override
+	public SampledDistribution infer(int[] evidenceDomainIndices)
+			throws Exception {
+		// set evidence
+		BeliefNode[] nodes = bn.bn.getNodes();
+		for(int i = 0; i < evidenceDomainIndices.length; i++)
+			if(evidenceDomainIndices[i] != -1)
+				nodes[i].setEvidence(new DiscreteEvidence(evidenceDomainIndices[i]));
+		
+		// run inference
+		edu.ksu.cis.bnj.ver3.inference.Inference algo = algorithmClass.newInstance();
+		algo.run(bn.bn);
+		
+		// retrieve results
+		createDistribution();
+		for(int i = 0; i < nodes.length; i++) {
+			CPF cpf = algo.queryMarginal(nodes[i]);
+			for(int j = 0; j < dist.values[i].length; j++)
+				dist.values[i][j] = cpf.getDouble(j);
+		}
+		dist.Z = 1.0;
+		dist.steps = 1;
+		dist.trials = 1;
+
+		// remove evidence
+		bn.removeAllEvidences();
+		
+		return dist;
+	}
+	
+}
