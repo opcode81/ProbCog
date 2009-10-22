5bb2e2c14a1473c3ed166eeb39c8eb1082d98dd0
jain <jain@cs.tum.edu>
2009-10-22 14:15:06 +0000

    added inference factory
    git-svn-id: svn+ssh://svnradig/probcog/trunk/SRLDB/src@1028 2a9d9b90-6428-0410-aa80-b0978a1f908e

--- src/BLNinfer.java
+++ src/BLNinfer.java
@@ -1,7 +1,6 @@
 import java.util.Vector;
 import java.util.regex.Pattern;
 
-
 import edu.ksu.cis.bnj.ver3.core.BeliefNode;
 import edu.ksu.cis.bnj.ver3.core.CPF;
 import edu.ksu.cis.bnj.ver3.core.values.ValueDouble;
@@ -12,14 +11,16 @@ import edu.tum.cs.bayesnets.inference.BackwardSamplingWithChildren;
 import edu.tum.cs.bayesnets.inference.BackwardSamplingWithPriors;
 import edu.tum.cs.bayesnets.inference.EnumerationAsk;
 import edu.tum.cs.bayesnets.inference.LikelihoodWeightingWithUncertainEvidence;
-import edu.tum.cs.bayesnets.inference.BNJInference;
 import edu.tum.cs.bayesnets.inference.SampleSearch;
 import edu.tum.cs.bayesnets.inference.SmileBackwardSampling;
 import edu.tum.cs.bayesnets.inference.SmileEPIS;
 import edu.tum.cs.srl.Database;
 import edu.tum.cs.srl.bayesnets.ABL;
-import edu.tum.cs.srl.bayesnets.bln.*;
+import edu.tum.cs.srl.bayesnets.bln.AbstractGroundBLN;
+import edu.tum.cs.srl.bayesnets.bln.BayesianLogicNetwork;
+import edu.tum.cs.srl.bayesnets.bln.GroundBLN;
 import edu.tum.cs.srl.bayesnets.bln.py.BayesianLogicNetworkPy;
+import edu.tum.cs.srl.bayesnets.inference.BLNInferenceFactory;
 import edu.tum.cs.srl.bayesnets.inference.BNSampler;
 import edu.tum.cs.srl.bayesnets.inference.CSPSampler;
 import edu.tum.cs.srl.bayesnets.inference.GibbsSampling;
@@ -31,12 +32,11 @@ import edu.tum.cs.srl.bayesnets.inference.SATIS;
 import edu.tum.cs.srl.bayesnets.inference.SATISEx;
 import edu.tum.cs.srl.bayesnets.inference.SATISExGibbs;
 import edu.tum.cs.srl.bayesnets.inference.Sampler;
+import edu.tum.cs.srl.bayesnets.inference.BLNInferenceFactory.Algorithm;
 import edu.tum.cs.util.Stopwatch;
 
 
 public class BLNinfer {
-
-	enum Algorithm {LikelihoodWeighting, LWU, CSP, GibbsSampling, EPIS, BackwardSampling, SmileBackwardSampling, BackwardSamplingPriors, BackwardSamplingWithChildren, Experimental, LiftedBackwardSampling, SATIS, SATISEx, EnumerationAsk, MCSAT, SATISExGibbs, Pearl, VarElim};
 	
 	/**
 	 * @param args
@@ -233,47 +233,7 @@ public class BLNinfer {
 			// run inference
 			Stopwatch sw = new Stopwatch();
 			sw.start();
-			Sampler sampler = null;
-			switch(algo) {
-			case LikelihoodWeighting: 
-				sampler = new LikelihoodWeighting(gbln); break;
-			case LWU: 
-				sampler = new BNSampler(gbln, LikelihoodWeightingWithUncertainEvidence.class); break;
-			case CSP: 
-				sampler = new CSPSampler(gbln); break;
-			case GibbsSampling:	
-				sampler = new GibbsSampling(gbln); break;
-			case EPIS:
-				sampler = new BNSampler(gbln, SmileEPIS.class); break;
-			case SmileBackwardSampling:
-				sampler = new BNSampler(gbln, SmileBackwardSampling.class); break;
-			case BackwardSampling:
-				sampler = new BNSampler(gbln, BackwardSampling.class); break;
-			case BackwardSamplingPriors:
-				sampler = new BNSampler(gbln, BackwardSamplingWithPriors.class); break;
-			case BackwardSamplingWithChildren:
-				sampler = new BNSampler(gbln, BackwardSamplingWithChildren.class); break;
-			case LiftedBackwardSampling:
-				sampler = new LiftedBackwardSampling(gbln); break;
-			case SATIS:
-				sampler = new SATIS((GroundBLN)gbln); break;
-			case SATISEx:
-				sampler = new SATISEx((GroundBLN)gbln); break;
-			case SATISExGibbs:
-				sampler = new SATISExGibbs((GroundBLN)gbln); break;
-			case MCSAT:
-				sampler = new MCSAT((GroundBLN)gbln); break;
-			case EnumerationAsk:
-				sampler = new BNSampler(gbln, EnumerationAsk.class); break;
-			case Pearl:
-				sampler = new BNSampler(gbln, BNJPearl.class); break;
-			case VarElim:
-				sampler = new BNSampler(gbln, BNJVariableElimination.class); break;
-			case Experimental:
-				sampler = new BNSampler(gbln, SampleSearch.class); break;
-			default: 
-				throw new Exception("algorithm not handled");
-			}
+			Sampler sampler = BLNInferenceFactory.createSampler(algo, gbln);
 			sampler.setDebugMode(debug);
 			if(sampler instanceof BNSampler) {
 				((BNSampler)sampler).setMaxTrials(maxTrials);
