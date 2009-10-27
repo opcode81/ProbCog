66dd27184b8544231f13630d56bd8df4f92a2c30
jain <jain@cs.tum.edu>
2009-10-27 19:37:39 +0000

    restructured SAT-IS algorithms to allow usage detached from GroundBLN objects
    git-svn-id: svn+ssh://svnradig/probcog/trunk/SRLDB/src@1058 2a9d9b90-6428-0410-aa80-b0978a1f908e

--- src/edu/tum/cs/srl/bayesnets/inference/SATISEx.java
+++ src/edu/tum/cs/srl/bayesnets/inference/SATISEx.java
@@ -1,10 +1,6 @@
 package edu.tum.cs.srl.bayesnets.inference;
 
-import edu.ksu.cis.bnj.ver3.core.BeliefNode;
-import edu.ksu.cis.bnj.ver3.core.CPF;
-import edu.tum.cs.bayesnets.core.BeliefNetworkEx;
-import edu.tum.cs.logic.Disjunction;
-import edu.tum.cs.logic.GroundLiteral;
+import edu.tum.cs.bayesnets.inference.SATIS_BSampler;
 import edu.tum.cs.logic.sat.ClausalKB;
 import edu.tum.cs.srl.bayesnets.bln.GroundBLN;
 
@@ -25,39 +21,8 @@ public class SATISEx extends SATIS {
 		ClausalKB ckb = super.getClausalKB();
 		
 		// extend the KB with formulas based on a CPD analysis
-		int size = ckb.size();
-		System.out.print("extending KB with deterministic constraints from CPDs... ");
-		BeliefNetworkEx bn = gbln.getGroundNetwork();
-		for(BeliefNode node : bn.bn.getNodes()) {
-			if(!gbln.isRegularVariable(node))
-				continue;
-			CPF cpf = node.getCPF();
-			BeliefNode[] domProd = cpf.getDomainProduct();
-			int[] addr = new int[domProd.length];
-			walkCPF4HardConstraints(cpf, addr, 0, ckb);
-		}
-		System.out.println((ckb.size()-size) + " constraints added");
+		SATIS_BSampler.extendKBWithDeterministicConstraintsInCPTs(gbln.getGroundNetwork(), gbln.getCoupling(), ckb);
 		
 		return ckb;
-	}
-
-	protected void walkCPF4HardConstraints(CPF cpf, int[] addr, int i, ClausalKB ckb) throws Exception {
-		BeliefNode[] domProd = cpf.getDomainProduct();
-		if(i == addr.length) {
-			double p = cpf.getDouble(addr);
-			if(p == 0.0) {
-				GroundLiteral[] lits = new GroundLiteral[domProd.length]; 
-				for(int k = 0; k < domProd.length; k++) {
-					lits[k] = gbln.getGroundLiteral(domProd[k], addr[k]);
-					lits[k].negate();
-				}
-				ckb.addFormula(new Disjunction(lits));
-			}
-			return;
-		}
-		for(int k = 0; k < domProd[i].getDomain().getOrder(); k++) {
-			addr[i] = k;
-			walkCPF4HardConstraints(cpf, addr, i+1, ckb);
-		}
 	}
 }
