22061d11c88eed4c524ca30a089459d43994c691
jain <jain@cs.tum.edu>
2009-11-04 15:42:38 +0000

    subclassed SampleSAT to allow initialization to use priors (which may improve performance)
    git-svn-id: svn+ssh://svnradig/probcog/trunk/SRLDB/src@1111 2a9d9b90-6428-0410-aa80-b0978a1f908e

--- src/edu/tum/cs/srl/bayesnets/inference/SATIS.java
+++ src/edu/tum/cs/srl/bayesnets/inference/SATIS.java
@@ -1,16 +1,22 @@
 package edu.tum.cs.srl.bayesnets.inference;
 
+import java.util.HashMap;
 import java.util.HashSet;
+import java.util.Random;
 
 import edu.ksu.cis.bnj.ver3.core.BeliefNode;
+import edu.tum.cs.bayesnets.core.BeliefNetworkEx;
 import edu.tum.cs.bayesnets.inference.SATIS_BSampler;
 import edu.tum.cs.bayesnets.inference.Sampler;
 import edu.tum.cs.logic.GroundLiteral;
 import edu.tum.cs.logic.PossibleWorld;
+import edu.tum.cs.logic.WorldVariables;
 import edu.tum.cs.logic.sat.ClausalKB;
 import edu.tum.cs.logic.sat.Clause;
 import edu.tum.cs.logic.sat.SampleSAT;
+import edu.tum.cs.srl.AbstractVariable;
 import edu.tum.cs.srl.bayesnets.bln.GroundBLN;
+import edu.tum.cs.srl.bayesnets.bln.coupling.VariableLogicCoupling;
 
 /**
  * SAT-IS: satisfiability-based importance sampling for inference in mixed networks with probabilistic and deterministic constraints
@@ -40,7 +46,9 @@ public class SATIS extends BNSampler {
 		// create SAT sampler
 		PossibleWorld state = new PossibleWorld(gbln.getWorldVars());
 		ClausalKB ckb = getClausalKB();
-		ss = new SampleSAT(ckb, state, gbln.getWorldVars(), gbln.getDatabase().getEntries());
+		//ss = new SampleSAT(ckb, state, gbln.getWorldVars(), gbln.getDatabase().getEntries());
+		ss = new SampleSATPriors(ckb, state, gbln.getWorldVars(), gbln.getDatabase().getEntries(), gbln.getGroundNetwork());
+		//ss.enableUnitPropagation();
 		
 		// get the set of variables that is determined by the sat sampler
 		determinedVars = new HashSet<BeliefNode>();
@@ -61,5 +69,30 @@ public class SATIS extends BNSampler {
 	@Override
 	protected Sampler getSampler() {
 		return new SATIS_BSampler(gbln.getGroundNetwork(), ss, gbln.getCoupling(), determinedVars);
+	}
+	
+	protected class SampleSATPriors extends SampleSAT {
+
+		BeliefNetworkEx bn;
+		HashMap<BeliefNode, double[]> priors = null;
+		Random generator;
+		
+		public SampleSATPriors(Iterable<? extends edu.tum.cs.logic.sat.Clause> kb, PossibleWorld state, WorldVariables vars, Iterable<? extends AbstractVariable> db, BeliefNetworkEx bn) throws Exception {
+			super(kb, state, vars, db);
+			this.bn = bn;
+			generator = new Random();
+		}
+		
+		protected void setRandomState() {
+			if(priors == null)
+				priors = bn.computePriors(evidenceDomainIndices);
+			VariableLogicCoupling coupling = gbln.getCoupling();
+			for(BeliefNode node : bn.bn.getNodes()) {
+				if(!coupling.hasCoupling(node))
+					continue;
+				int domIdx = Sampler.sample(priors.get(node), generator);
+				coupling.setVariableValue(node, domIdx, this.state);
+			}
+		}
 	}
 }
