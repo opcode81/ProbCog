e45d973d6a44c2a5847de2cee9ecf11b57a2b46d
jain <jain@cs.tum.edu>
2009-11-17 12:17:24 +0000

    added BLN2MLN
    git-svn-id: svn+ssh://svnradig/probcog/trunk/SRLDB/src@1139 2a9d9b90-6428-0410-aa80-b0978a1f908e

---
+++ src/BLN2MLN.java
@@ -0,0 +1,43 @@
+import java.io.File;
+
+import edu.tum.cs.srl.bayesnets.ABL;
+import edu.tum.cs.srl.bayesnets.RelationalBeliefNetwork;
+import edu.tum.cs.srl.bayesnets.bln.BayesianLogicNetwork;
+import edu.tum.cs.srl.mln.MarkovLogicNetwork;
+
+
+public class BLN2MLN {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		try {
+			if(args.length < 4) {
+				System.out.println("\n usage: BLN2MLN <declarations file> <network file> <logic file> <output file> [options] \n\n" + 
+						             "        -cf  write compact formulas\n" 
+						          );
+				return;
+			}			
+			boolean compact = false;
+			for(int i = 4; i < args.length; i++) {
+				if(args[i].equals("-cf"))
+					compact = true;
+				else {
+					System.err.println("unknown option " + args[i]);
+					return;
+				}
+			}
+			RelationalBeliefNetwork rbn = new ABL(args[0], args[1]);
+			BayesianLogicNetwork bln = new BayesianLogicNetwork(rbn, args[2]);
+			String outfile = args[3];
+			System.out.println("converting...");
+			MarkovLogicNetwork mln = bln.toMLN();
+			System.out.printf("saving MLN to %s...\n", outfile);
+			mln.write(new File(outfile));
+		}
+		catch(Exception e) {
+			e.printStackTrace();
+		}
+	}
+}
