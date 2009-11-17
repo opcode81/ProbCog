d5c8d6aceb2a80f861e033cb2648713c33d5d19a
jain <jain@cs.tum.edu>
2009-11-17 12:42:29 +0000

    new MLN conversion code
    git-svn-id: svn+ssh://svnradig/probcog/trunk/SRLDB/src@1149 2a9d9b90-6428-0410-aa80-b0978a1f908e

--- src/edu/tum/cs/srl/bayesnets/bln/BayesianLogicNetwork.java
+++ src/edu/tum/cs/srl/bayesnets/bln/BayesianLogicNetwork.java
@@ -2,10 +2,13 @@ package edu.tum.cs.srl.bayesnets.bln;
 
 import java.io.IOException;
 
+import edu.tum.cs.logic.Formula;
 import edu.tum.cs.logic.KnowledgeBase;
 import edu.tum.cs.logic.parser.ParseException;
 import edu.tum.cs.srl.Database;
+import edu.tum.cs.srl.bayesnets.MLNConverter;
 import edu.tum.cs.srl.bayesnets.RelationalBeliefNetwork;
+import edu.tum.cs.srl.mln.MarkovLogicNetwork;
 
 public class BayesianLogicNetwork extends AbstractBayesianLogicNetwork {
 
@@ -20,5 +23,14 @@ public class BayesianLogicNetwork extends AbstractBayesianLogicNetwork {
 		GroundBLN gbln = new GroundBLN(this, db);
 		gbln.instantiateGroundNetwork();
 		return gbln;
+	}
+	
+	public MarkovLogicNetwork toMLN() throws Exception {
+		MLNConverter.MLNObjectWriter converter = new MLNConverter.MLNObjectWriter();
+		this.rbn.toMLN(converter, false, false);
+		for(Formula f : kb) {
+			converter.addHardFormula(f);
+		}
+		return converter.getMLN();
 	}
 }
