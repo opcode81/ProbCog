1c8e374babca3889e4ed473532d107a2dc362c22
jain <jain@2a9d9b90-6428-0410-aa80-b0978a1f908e>
2009-12-17 15:39:52 +0000

    save and load only relevant subobjects (less problematic when modifying classes)
    git-svn-id: svn+ssh://svnradig/probcog/trunk/SRLDB/src@1193 2a9d9b90-6428-0410-aa80-b0978a1f908e

--- src/edu/tum/cs/inference/GeneralSampledDistribution.java
+++ src/edu/tum/cs/inference/GeneralSampledDistribution.java
@@ -12,7 +12,6 @@ import java.io.FileOutputStream;
 import java.io.IOException;
 import java.io.ObjectInputStream;
 import java.io.ObjectOutputStream;
-import java.io.Serializable;
 import java.util.HashMap;
 
 /**
@@ -20,9 +19,8 @@ import java.util.HashMap;
  * Any BasicSampledDistribution can be converted to this type of object.
  * @author jain
  */
-public class GeneralSampledDistribution extends BasicSampledDistribution implements Serializable {
+public class GeneralSampledDistribution extends BasicSampledDistribution {
 
-	private static final long serialVersionUID = 1L;
 	protected String[] varNames;
 	protected String[][] domains;
 	protected HashMap<String,Integer> varName2Index;
@@ -56,7 +54,10 @@ public class GeneralSampledDistribution extends BasicSampledDistribution impleme
 	public void write(File f) throws IOException {
 		FileOutputStream fos = new FileOutputStream(f);
 		ObjectOutputStream oos = new ObjectOutputStream(fos);
-		oos.writeObject(this);
+		oos.writeObject(this.values);
+		oos.writeObject(this.Z);
+		oos.writeObject(varNames);
+		oos.writeObject(domains);
 		oos.close();
 	}
 	
@@ -69,9 +70,12 @@ public class GeneralSampledDistribution extends BasicSampledDistribution impleme
 	 */
 	public static GeneralSampledDistribution fromFile(File f) throws IOException, ClassNotFoundException {
 		java.io.ObjectInputStream objstream = new ObjectInputStream(new FileInputStream(f));
-	    java.lang.Object object = objstream.readObject();
+	    double[][] values = (double[][])objstream.readObject();
+	    Double Z = (Double)objstream.readObject();
+	    String[] varNames = (String[])objstream.readObject();
+	    String[][] domains = (String[][])objstream.readObject();
 	    objstream.close();
-	    return (GeneralSampledDistribution)object;
+	    return new GeneralSampledDistribution(values, Z, varNames, domains);
 	}
 
 	@Override
