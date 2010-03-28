9412344a0604a8c77ef1101ac7e8e57eb9e5a277
jain <jain@cs.tum.edu>
2010-03-28 16:31:17 +0000

    updated experiments
    git-svn-id: svn+ssh://svnradig/probcog/trunk/SRLDB/python@1328 2a9d9b90-6428-0410-aa80-b0978a1f908e

--- python/testMCSATPC.py
+++ python/testMCSATPC.py
@@ -1,6 +1,7 @@
-from MLN import MLN
+from MLN import MLN, InferenceMethods
 from math import exp, log
 import random
+import pickle
 
 class StringBuffer:
 	def __init__(self):
@@ -37,30 +38,56 @@ def createRandomMLN(numVars, numFeatures, maxCliqueSize, maxWeight, numProbConst
 		f.write("0 %s\n" % v)
 	return (vars, str(f), pcVars)
 
-def test(numExperiments, numVars, numFeatures, maxCliqueSize, maxWeight, numProbConstraints):
+def ipfpm_vs_mcsatpc(numExperiments, numVars, numFeatures, maxCliqueSize, maxWeight, numProbConstraints, ipfpmSubAlgorithm, ipfpmSubParams):
 	ret = []
+	# collect data
 	for i in xrange(numExperiments):
 		print "\nExperiment %d" % (i+1)
 		
 		# create MLN
 		vars, mlnContent, pcVars = createRandomMLN(numVars, numFeatures, maxCliqueSize, maxWeight, numProbConstraints)
 		#print mlnContent
+		queryVars = list(set(vars).difference(pcVars))
 		
 		# run IPFP-M
 		mln = MLN(mlnContent=mlnContent, verbose=False)
 		mln.combine({})	
-		ipfpmResults = mln.inferIPFPM(vars)
+		ipfpmResults = mln.inferIPFPM(queryVars, details=True, inferenceMethod=ipfpmSubAlgorithm, inferenceParams=ipfpmSubParams)
+		ipfpmData = mln.ipfpm.data
 		print "  %s" % ipfpmResults
 		
-		# run MC-SAT-PC
-		
+		# run MC-SAT-PC		
 		mln = MLN(mlnContent=mlnContent, verbose=False)
 		mln.combine({})	
-		mcsatpcResults = mln.inferMCSAT(vars, maxSteps=10000, infoInterval=1000, resultsInterval=1000, verbose=False, details=False, keepResultsHistory=True, referenceResults=ipfpmResults)
+		mcsatpcResults = mln.inferMCSAT(queryVars, maxSteps=10000, infoInterval=1000, resultsInterval=1000, verbose=False, details=False, keepResultsHistory=True, referenceResults=ipfpmResults, p=0.5)
+		mcsatpcHistory = mln.mcsat.getResultsHistory()
+		final = mcsatpcHistory[-1]
 		print "  %s" % mcsatpcResults
-		ret.append(mln.mcsat.getResultsHistory())
+		print " mean error: %f, max error: %f" % (final["reference_me"], final["reference_maxe"])		
+		ret.append({"mlnContent": mlnContent, "mcsatpc": mcsatpcHistory, "ipfpm": {"results": ipfpmResults, "data": ipfpmData}})
 	
 	return ret
 
+'''
+How good is MC-SAT-PC in comparison to IPFP-M[exact] on small Ns?
+How good is MC-SAT-PC in comparison to IPFP-M[MC-SAT] on larger Ns?
+How does MC-SAT-PC compare to MC-SAT - do we need more steps to converge?
+How good is MC-SAT-PC in our real-world example (entity res.) compared to IPFP-M[MC-SAT]?
+
+Try implementing convergence of MC-SAT within IPFP-M[MC-SAT] (based computed MCMC probability of the formula that was last fitted)
+'''
+
+def collectData(numExperiments, numVars, useMCSAT):
+	if useMCSAT:
+		ipfpmSubAlgorithm = InferenceMethods.MCSAT
+		ipfpmSubParams = dict(maxSteps=10000)
+	else:
+		ipfpmSubAlgorithm = InferenceMethods.exact
+		ipfpmSubParams = None
+	N = numVars
+	data = ipfpm_vs_mcsatpc(1, N, N, min(N,10), log(50), N/2, ipfpmSubAlgorithm, ipfpmSubParams)
+	filename = "ipfpm(%s)_vs_mcsatpc_%dvars.pickle" % (InferenceMethods._names[ipfpmSubAlgorithm], N)
+	pickle.dump(data, file(filename, "wb"))
+
 if __name__=='__main__':
-	test(1, 4, 4, 4, log(50), 2)
+	collectData(4, 4, False)
