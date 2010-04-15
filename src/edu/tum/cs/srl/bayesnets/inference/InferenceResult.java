1b00ecf4f56c247f5d82e336e8e41ec229a5a8fc
jain <jain@cs.tum.edu>
2010-04-15 14:31:38 +0000

    added confidence intervals
    git-svn-id: svn+ssh://svnradig/probcog/trunk/SRLDB/src@1363 2a9d9b90-6428-0410-aa80-b0978a1f908e

--- src/edu/tum/cs/srl/bayesnets/inference/InferenceResult.java
+++ src/edu/tum/cs/srl/bayesnets/inference/InferenceResult.java
@@ -1,33 +1,40 @@
 package edu.tum.cs.srl.bayesnets.inference;
 
 import edu.ksu.cis.bnj.ver3.core.BeliefNode;
-import edu.ksu.cis.bnj.ver3.core.Discrete;
 import edu.tum.cs.bayesnets.inference.SampledDistribution;
+import edu.tum.cs.inference.BasicSampledDistribution;
 
 public class InferenceResult implements Comparable<InferenceResult> {
 	public String varName;
 	public String[] domainElements;
 	public double[] probabilities;
+	public Object[] additionalInfo = null;
 	
 	public InferenceResult(SampledDistribution dist, int nodeIdx) {
 		BeliefNode node = dist.bn.bn.getNodes()[nodeIdx];
 		varName = node.getName();
-		Discrete domain = (Discrete)node.getDomain();
-		int dim = domain.getOrder();
-		domainElements = new String[dim];
-		probabilities = new double[dim];
-		for(int j = 0; j < dim; j++) {
-			domainElements[j] = domain.getName(j);
-			probabilities[j] = dist.getProbability(nodeIdx, j); 
+		domainElements = dist.getDomain(nodeIdx);
+		probabilities = dist.getDistribution(nodeIdx);
+		if(dist.usesConfidenceComputation()) {
+			additionalInfo = new BasicSampledDistribution.ConfidenceInterval[domainElements.length];
+			for(int i = 0; i < additionalInfo.length; i++)
+				additionalInfo[i] = dist.getConfidenceInterval(nodeIdx, i);
 		}
 	}
 	
 	public void print() {
 		System.err.println(varName + ":");
-		for(int i = 0; i < domainElements.length; i++)
-			System.err.println(String.format("  %.4f %s", probabilities[i], domainElements[i]));
+		if(additionalInfo == null)
+			for(int i = 0; i < domainElements.length; i++)
+				System.out.println(String.format("  %.4f %s", probabilities[i], domainElements[i]));
+		else
+			for(int i = 0; i < domainElements.length; i++)
+				System.out.println(String.format("  %.4f  %s  %s", probabilities[i], additionalInfo[i], domainElements[i]));
 	}
 
+	/**
+	 * for ordering inference results according to alphabetical ordering of variable names
+	 */
 	public int compareTo(InferenceResult o) {
 		return varName.compareTo(o.varName);
 	}
