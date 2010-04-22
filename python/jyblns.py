29f8a0f615424bdc21e4f1e72477cc2aa602133f
jain <jain@2a9d9b90-6428-0410-aa80-b0978a1f908e>
2010-04-22 12:22:26 +0000

    changed interface of bln inference
    git-svn-id: svn+ssh://svnradig/probcog/trunk/SRLDB/python@1419 2a9d9b90-6428-0410-aa80-b0978a1f908e

--- python/jyblns.py
+++ python/jyblns.py
@@ -16,7 +16,7 @@ importlib("optimization.jar")
 
 from edu.tum.cs.srl.bayesnets.inference import BLNinfer
 
-def infer(network, decls, logic, algorithm, evidence, queries, cwPreds = None, maxSteps = None, *moreargs):
+def infer(network, decls, logic, algorithm, evidence, queries, cwPreds = None, maxSteps = None, args=None):
 	'''
 		network: the model's fragment network (filename)
 		decls: the model's declarations (filename)
@@ -26,24 +26,24 @@ def infer(network, decls, logic, algorithm, evidence, queries, cwPreds = None, m
 		queries: a comma-separated list of queries (predicate names or fully or partially grounded atoms)
 		cwPreds: a comma-separated list of closed-world predicates
 		maxSteps: maximum number of steps to take		
-		(moreargs: may specify any additional command line arguments that the BLNinfer java program can process, see command-line help)
+		args: list of additional arguments to pass (see BLNinfer help)
 		returns the BLNinfer object, which can be used to immediately obtain the results via .getResults() and other information
 	'''
-	args = ['-ia', algorithm, '-x', network, '-b', decls, '-l', logic, '-e', evidence, '-q', queries]
+	a = ['-ia', algorithm, '-x', network, '-b', decls, '-l', logic, '-e', evidence, '-q', queries]
 	if cwPreds is not None:
-		args.extend(["-cw", cwPreds])
+		a.extend(["-cw", cwPreds])
 	if maxSteps is not None:
-		args.extend(["-maxSteps", str(maxSteps)])
-	args.extend(moreargs)
+		a.extend(["-maxSteps", str(maxSteps)])
+	a.extend(args)
 	i = BLNinfer()
-	i.readArgs(args)
+	i.readArgs(a)
 	i.run()
 	return i
 	
 # example usage
 if __name__=='__main__':
 	p = "../models/kitchen/tableSetting/"
-	inf = infer(p+"meals_any_for_functional.adUt30w.learnt.xml", p+"meals_any_for.adUt.learnt.blog", p+"meals_any_for_functional.bln", 'LikelihoodWeighting', p+"rssdejan_scene4.blogdb", "usesAnyIn,consumesAnyIn,name", None, None, "--confidenceLevel=0.95")
+	inf = infer(p+"meals_any_for_functional.adUt30w.learnt.xml", p+"meals_any_for.adUt.learnt.blog", p+"meals_any_for_functional.bln", 'LikelihoodWeighting', p+"rssdejan_scene4.blogdb", "usesAnyIn,consumesAnyIn,name", args=["--confidenceLevel=0.95"])
 	for r in inf.getResults():
 		print "%s" % r.varName
 		for i in range(r.getDomainSize()):
