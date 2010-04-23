b17463ae33394e806ca5fd19c1ceb250ad0f9ace
jain <jain@cs.tum.edu>
2010-04-23 11:36:25 +0000

    todos for JPL-bases learning
    git-svn-id: svn+ssh://svnradig/probcog/trunk/SRLDB/src@1424 2a9d9b90-6428-0410-aa80-b0978a1f908e

---
+++ src/BLNlearnJPL.java
@@ -0,0 +1,21 @@
+import edu.tum.cs.probcog.BLNModel;
+
+/*
+ * Created on Apr 23, 2010
+ *
+ * TODO To change the template for this generated file go to
+ * Window - Preferences - Java - Code Style - Code Templates
+ */
+
+public class BLNlearnJPL {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		//BLNModel blnmodel = new BLNModel()
+		//blnmodel.learnViaJPL(prologModule);
+	}
+
+}
