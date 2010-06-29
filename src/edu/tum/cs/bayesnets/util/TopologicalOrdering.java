435a9c2f1880f5b5caaa8c481172b1efb94c222e
jain <jain@2a9d9b90-6428-0410-aa80-b0978a1f908e>
2010-06-29 15:57:19 +0000

    cleaned up, added new methods
    git-svn-id: svn+ssh://svnradig/probcog/trunk/SRLDB/src@1574 2a9d9b90-6428-0410-aa80-b0978a1f908e

--- src/edu/tum/cs/bayesnets/util/TopologicalOrdering.java
+++ src/edu/tum/cs/bayesnets/util/TopologicalOrdering.java
@@ -1,67 +1,95 @@
 package edu.tum.cs.bayesnets.util;
 
 import java.util.HashMap;
-import java.util.Iterator;
 import java.util.NoSuchElementException;
 import java.util.Random;
 import java.util.Vector;
 
 import edu.ksu.cis.bnj.ver3.core.BeliefNode;
 
-public class TopologicalOrdering implements Iterable<Integer>, Iterator<Integer> {
-	
+public class TopologicalOrdering implements Iterable<Integer> {
+	/**
+	 * a vector of tiers reflecting the partial ordering of the nodes; the nodes in each tier (sub-vector) are equivalent with respect to the ordering
+	 */
 	Vector<Vector<Integer>> partialOrder;
-	int idxCurrentLevel;
-	Vector<Integer> currentLevel;
+	/**
+	 * a mapping from belief nodes to 0-based tier indices (i.e. depth in the ordering)
+	 */
 	HashMap<BeliefNode, Integer> tierMap;
-	Random random;
-	
-	public TopologicalOrdering(Vector<Vector<Integer>> partialOrder, HashMap<BeliefNode, Integer> tierMap, Random rand) {
-		this.partialOrder = partialOrder;
-		this.tierMap = tierMap;
-		this.random = rand; 
-		reset();
-	}
 	
 	public TopologicalOrdering(Vector<Vector<Integer>> partialOrder, HashMap<BeliefNode, Integer> tierMap) {
-		this(partialOrder, tierMap, new Random());
-	}
-	
-	public void reset() {
-		idxCurrentLevel = 0;
-		currentLevel = new Vector<Integer>(partialOrder.get(0));
+		this.partialOrder = partialOrder;
+		this.tierMap = tierMap;
 	}
 
-	public Iterator<Integer> iterator() {
-		return this;
+	public java.util.Iterator<Integer> iterator() {
+		return new Iterator(this);
 	}
 
-	public boolean hasNext() {
-		return currentLevel != null && !currentLevel.isEmpty();
-	}
+	protected static class Iterator implements java.util.Iterator<Integer> {
 
-	public Integer next() {
-		if(!hasNext())
-			throw new NoSuchElementException();
-		int idxNext = random.nextInt(currentLevel.size());
-		int nextElem = currentLevel.remove(idxNext);
-		if(currentLevel.isEmpty()) {
-			++idxCurrentLevel;
-			if(idxCurrentLevel < partialOrder.size())
-				currentLevel = new Vector<Integer>(partialOrder.get(idxCurrentLevel));
-			else
-				currentLevel = null;
+		int idxCurrentLevel;
+		Vector<Integer> currentLevel;
+		Random random;
+		TopologicalOrdering ordering;
+		
+		public Iterator(TopologicalOrdering ordering) {
+			this.ordering = ordering;
+			idxCurrentLevel = 0;
+			currentLevel = new Vector<Integer>(ordering.partialOrder.get(0));
+			random = new Random();
 		}
-		return nextElem;
-	}
+		
+		public boolean hasNext() {
+			return currentLevel != null && !currentLevel.isEmpty();
+		}
 	
+		public Integer next() {
+			if(!hasNext())
+				throw new NoSuchElementException();
+			int idxNext = random.nextInt(currentLevel.size());
+			int nextElem = currentLevel.remove(idxNext);
+			if(currentLevel.isEmpty()) {
+				++idxCurrentLevel;
+				if(idxCurrentLevel < ordering.partialOrder.size())
+					currentLevel = new Vector<Integer>(ordering.partialOrder.get(idxCurrentLevel));
+				else
+					currentLevel = null;
+			}
+			return nextElem;
+		}
+		
+		public void remove() {
+			throw new RuntimeException("remove() is not supported by this constant iterator.");
+		}
+
+	}
+
+	/**
+	 * gets the tier (i.e. depth in the ordering) of the given node
+	 * @param n a belief node
+	 * @return 0-based tier
+	 */
 	public int getTier(BeliefNode n) {
 		if(tierMap == null)
 			throw new RuntimeException("Topological ordering has no tier map!");
 		return tierMap.get(n);
 	}
 
-	public void remove() {
-		throw new RuntimeException("remove() is not supported by this constant iterator.");
+	public int getNumTiers() {
+		return partialOrder.size();
+	}
+	
+	/**
+	 * gets the vector of indices of nodes belonging to the given tier
+	 * @param index
+	 * @return
+	 */
+	public Vector<Integer> getTier(int index) {
+		return partialOrder.get(index);
+	}
+	
+	public Vector<Vector<Integer>> getTiers() {
+		return partialOrder;
 	}
 }
