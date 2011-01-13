ba1c010d0fdf06012e255b1af436ebc91413c124
jain <jain@cs.tum.edu>
2011-01-13 19:47:23 +0000

    started refactoring MLN.py into many separate modules
    git-svn-id: svn+ssh://svnradig/probcog/trunk/SRLDB/python@1852 2a9d9b90-6428-0410-aa80-b0978a1f908e

---
+++ python/MLN/inference/ExactInference.py
@@ -0,0 +1,166 @@
+# -*- coding: iso-8859-1 -*-
+#
+# Markov Logic Networks
+#
+# (C) 2006-2010 by Dominik Jain (jain@cs.tum.edu)
+# 
+# Permission is hereby granted, free of charge, to any person obtaining
+# a copy of this software and associated documentation files (the
+# "Software"), to deal in the Software without restriction, including
+# without limitation the rights to use, copy, modify, merge, publish,
+# distribute, sublicense, and/or sell copies of the Software, and to
+# permit persons to whom the Software is furnished to do so, subject to
+# the following conditions:
+#
+# The above copyright notice and this permission notice shall be
+# included in all copies or substantial portions of the Software.
+#
+# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
+# EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF
+# MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT.
+# IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY
+# CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT,
+# TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE
+# SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+
+from Inference import *
+
+class ExactInference(Inference):
+    def __init__(self, mln):
+        Inference.__init__(self, mln)
+    
+    # verbose: whether to print results (or anything at all, in fact)
+    # details: (given that verbose is true) whether to output additional status information
+    # debug: (given that verbose is true) if true, outputs debug information, in particular the distribution over possible worlds
+    # debugLevel: level of detail for debug mode
+    def _infer(self, verbose=True, details=False, shortOutput=False, debug=False, debugLevel=1, **args):
+        worldValueKey = {"weights": "sum", "probs": "prod"}[self.mln.parameterType]
+        # get set of possible worlds along with their probabilities
+        if verbose and details: print "generating possible worlds..."
+        self.mln._getWorlds() 
+        if verbose and details: print "  %d worlds instantiated" % len(self.mln.worlds)
+        # get the query formula(s)
+        what = self.queries
+        # if we have no explicit evidence, get the evidence that is set in the MLN as a conjunction
+        given = self.given
+        if given is None:
+            given = evidence2conjunction(self.mln.getEvidenceDatabase())        
+        # ground the evidence formula
+        if given == "":
+            given = None
+        else:
+            given = FOL.parseFormula(given)
+            given = given.ground(self.mln, {})
+        # start summing
+        if verbose and details: print "summing..."
+        numerators = [0.0 for i in range(len(what))]
+        denominator = 0
+        k = 1
+        numerator_worlds = [[] for i in range(len(what))]
+        denominator_worlds = []
+        for world in self.mln.worlds:
+            precond = (given == None)
+            if not precond:
+                precond = given.isTrue(world["values"])
+            if precond:
+                for i in range(len(what)):
+                    if what[i].isTrue(world["values"]):
+                        numerators[i] += world[worldValueKey]
+                        numerator_worlds[i].append(k)
+                denominator += world[worldValueKey]
+                denominator_worlds.append(k)
+            k += 1
+        # normalize answers
+        answers = []
+        for i in range(len(what)):
+            answers.append(numerators[i] / denominator)
+        # some debug info
+        if debug and verbose:  
+            self.mln.printWorlds()
+            for i in range(len(what)):
+                self.additionalQueryInfo[i] = "\n   %s / %s " % (numerator_worlds[i], denominator_worlds)
+        # return results
+        return answers
+
+class ExactInferenceLazy(Inference):
+    '''
+    variant of exact inference, where the possible worlds and associated values are generated dynamically (on demand);
+    In particular, possible worlds are not kept in memory, such that memory consumption remains linear in the number of variables.    
+    '''
+    
+    def __init__(self, mln):
+        Inference.__init__(self, mln)
+    
+    # verbose: whether to print results (or anything at all, in fact)
+    # details: (given that verbose is true) whether to output additional status information
+    # debug: (given that verbose is true) if true, outputs debug information, in particular the distribution over possible worlds
+    # debugLevel: level of detail for debug mode
+    def _infer(self, verbose=True, details=False, shortOutput=False, debug=False, debugLevel=1, **args):
+        # get the query formula(s)
+        what = self.queries
+        # if we have no explicit evidence, get the evidence that is set in the MLN as a conjunction
+        given = self.given
+        if given is None:
+            given = evidence2conjunction(self.mln.getEvidenceDatabase())        
+        # ground the evidence formula
+        given = FOL.parseFormula(given)
+        given = given.ground(self.mln, {})
+        # start summing
+        if verbose and details: print "summing..."
+        wts = self.mln._weights()
+        numerators = [0.0 for i in xrange(len(what))]
+        denominator = 0
+        k = 1
+        for world in self._iterWorlds([], 0):
+            precond = (given == None)
+            if not precond:
+                precond = given.isTrue(world)
+            if precond:
+                # compute world value
+                worldValue = self.mln._calculateWorldExpSum(world, wts)
+                # add to applicable numerators
+                for i in xrange(len(what)):
+                    if what[i].isTrue(world):
+                        numerators[i] += worldValue
+                # add to denominator
+                denominator += worldValue
+            k += 1
+        # normalize answers
+        answers = []
+        for i in range(len(what)):
+            answers.append(numerators[i] / denominator)
+        return answers
+    
+    def _iterWorlds(self, values, idx):
+        mln = self.mln
+        if idx == len(mln.gndAtoms):
+            yield values
+        else:     
+            # values that can be set for the truth value of the ground atom with index idx
+            possible_settings = [True, False]
+            # check for rigid predicates: for rigid predicates, we consider both values only if the evidence value is
+            # unknown, otherwise we use the evidence value
+            restricted = False
+            gndAtom = mln.gndAtomsByIdx[idx]
+            # check if setting the truth value for idx is critical for a block (which is the case when idx is the highest index in a block)
+            if idx in mln.gndBlockLookup and POSSWORLDS_BLOCKING:
+                block = mln.gndBlocks[mln.gndBlockLookup[idx]]
+                if idx == max(block):
+                    # count number of true values already set
+                    nTrue, nFalse = 0, 0
+                    for i in block:
+                        if i < len(values): # i has already been set
+                            if values[i]:
+                                nTrue += 1
+                    if nTrue >= 2: # violation, cannot continue
+                        return
+                    if nTrue == 1: # already have a true value, must set current value to false
+                        possible_settings.remove(True)
+                    if nTrue == 0: # no true value yet, must set current value to true
+                        possible_settings.remove(False)
+            # recursive descent
+            for x in possible_settings:
+                values.append(x)
+                for w in self._iterWorlds(values, idx + 1):
+                    yield w
+                values.pop()
---
+++ python/MLN/inference/Inference.py
@@ -0,0 +1,146 @@
+# -*- coding: iso-8859-1 -*-
+#
+# Markov Logic Networks
+#
+# (C) 2006-2010 by Dominik Jain (jain@cs.tum.edu)
+# 
+# Permission is hereby granted, free of charge, to any person obtaining
+# a copy of this software and associated documentation files (the
+# "Software"), to deal in the Software without restriction, including
+# without limitation the rights to use, copy, modify, merge, publish,
+# distribute, sublicense, and/or sell copies of the Software, and to
+# permit persons to whom the Software is furnished to do so, subject to
+# the following conditions:
+#
+# The above copyright notice and this permission notice shall be
+# included in all copies or substantial portions of the Software.
+#
+# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
+# EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF
+# MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT.
+# IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY
+# CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT,
+# TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE
+# SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+
+import time
+import sys
+
+import FOL
+from MLN.util import *
+
+
+class Inference(object):
+    def __init__(self, mln):
+        self.mln = mln
+        self.t_start = time.time()
+    
+    def _readQueries(self, queries):
+        # check for single/multiple query and expand
+        if type(queries) != list:
+            queries = [queries]
+        queries = self.mln._expandQueries(queries)
+        # parse queries that aren't already formulas
+        self.queries = []
+        for q in queries:
+            if isinstance(q, FOL.Formula):
+                self.queries.append(q)
+            else:
+                q = FOL.parseFormula(q)
+                self.queries.append(q.ground(self.mln, {}))
+    
+    # set evidence in the MLN according to the given conjunction of ground literals
+    def _setEvidence(self, conjunction):
+        if conjunction is not None:
+            literals = map(lambda x: x.strip().replace(" ", ""), conjunction.split("^"))
+            evidence = {}
+            for gndAtom in literals:
+                if gndAtom == '': continue
+                tv = True
+                if(gndAtom[0] == '!'):
+                    tv = False
+                    gndAtom = gndAtom[1:]
+                evidence[gndAtom] = tv
+            self.mln.setEvidence(evidence)
+
+    def _getElapsedTime(self):
+        ''' returns a pair (t,s) where t is the time in seconds elapsed thus far (since construction) and s is a readable string representation thereof '''
+        elapsed = time.time() - self.t_start
+        hours = int(elapsed / 3600)
+        elapsed -= hours * 3600
+        minutes = int(elapsed / 60)
+        elapsed -= minutes * 60
+        secs = int(elapsed)
+        msecs = int((elapsed - secs) * 1000)
+        return (elapsed, "%d:%02d:%02d.%03d" % (hours, minutes, secs, msecs))
+
+    def infer(self, queries, given=None, verbose=True, details=False, shortOutput=False, outFile=None, **args):
+        '''
+            queries: a list of queries - either strings (predicate names or partially/fully grounded atoms) or ground formulas
+        '''
+        self.given = given
+        # read queries
+        self._readQueries(queries)
+        self.additionalQueryInfo = [""] * len(self.queries)
+        # perform actual inference (polymorphic)
+        print type(self)
+        self.results = self._infer(verbose=verbose, details=details, **args)
+        self.totalInferenceTime = self._getElapsedTime()
+        # output
+        if verbose:
+            if details: print "\nresults:"
+            self.writeResults(sys.stdout, shortOutput=shortOutput)
+        if outFile != None:
+            self.writeResults(outFile, shortOutput=True)
+        # return results
+        if len(self.queries) > 1:
+            return self.results
+        else:
+            return self.results[0]
+    
+    def getTotalInferenceTime(self):
+        ''' returns a pair (t,s) where t is the total inference time in seconds and s is a readable string representation thereof '''
+        return self.totalInferenceTime
+    
+    def writeResults(self, out, shortOutput=True):
+        self._writeResults(out, self.results, shortOutput)
+    
+    def _writeResults(self, out, results, shortOutput=True):
+        # determine maximum query length to beautify output
+        if shortOutput:
+            maxLen = 0
+            for q in self.queries:
+                maxLen = max(maxLen, len(strFormula(q)))
+        # if necessary, get string representation of evidence
+        if not shortOutput:
+            if self.given is not None:
+                evidenceString = self.given
+            else:
+                evidenceString = evidence2conjunction(self.mln.getEvidenceDatabase())
+        # print sorted results, one per line
+        strQueries = map(strFormula, self.queries)
+        query2Index = {}
+        for i, q in enumerate(strQueries): query2Index[q] = i
+        for q in sorted(strQueries):
+            i = query2Index[q]
+            addInfo = self.additionalQueryInfo[i]
+            if not shortOutput:
+                out.write("P(%s | %s) = %f  %s\n" % (q, evidenceString, results[i], addInfo))
+            else:
+                out.write("%f  %-*s  %s\n" % (results[i], maxLen, q, addInfo))
+    
+    def _compareResults(self, results, referenceResults):
+        if len(results) != len(referenceResults):
+            raise Exception("Cannot compare results - different number of results in reference")
+        me = 0
+        mse = 0
+        maxdiff = 0
+        for i in xrange(len(results)):
+            diff = abs(results[i] - referenceResults[i])
+            maxdiff = max(maxdiff, diff)
+            me += diff
+            mse += diff * diff
+        me /= len(results)
+        mse /= len(results)
+        return {"reference_me": me, "reference_mse": mse, "reference_maxe": maxdiff}
+
