adb1b85905e52fe1290c9c7c9f46479483f66d24
jain <jain@cs.tum.edu>
2011-01-13 19:52:49 +0000

    added missing files
    git-svn-id: svn+ssh://svnradig/probcog/trunk/SRLDB/python@1853 2a9d9b90-6428-0410-aa80-b0978a1f908e

---
+++ python/MLN/inference/__init__.py
@@ -0,0 +1,6 @@
+
+from ExactInference import ExactInference, ExactInferenceLazy
+from MCSAT import MCSAT, SampleSAT
+from GibbsSampler import GibbsSampler
+from IPFPM import IPFPM
+from SAMaxWalkSAT import SAMaxWalkSAT
---
+++ python/MLN/methods.py
@@ -0,0 +1,21 @@
+
+class InferenceMethods:
+    exact, GibbsSampling, MCSAT, exactLazy, IPFPM_exact, IPFPM_MCSAT = range(6)
+    _names = {exact: "exact inference", GibbsSampling: "Gibbs sampling", MCSAT: "MC-SAT", exactLazy: "lazy exact inference", IPFPM_exact: "IPFP-M[exact]", IPFPM_MCSAT: "IPFP-M[MC-SAT]"}
+    _byName = dict([(x, y) for (y, x) in _names.iteritems()])
+    
+class ParameterLearningMeasures:
+    LL, PLL, BPLL, PLL_fixed, BPLL_fixed, NPL_fixed, LL_ISE, PLL_ISE, LL_ISEWW, SLL_ISE = range(10)
+    _names = {LL: "log-likelihood",
+              PLL: "pseudo-log-likelihood",
+              BPLL: "pseudo-log-likelihood with blocking",
+              PLL_fixed: "pseudo-log-likelihood with fixed unitary clauses",
+              BPLL_fixed: "pseudo-log-likelihood with blocking and fixed unitary clauses",
+              NPL_fixed: "negative pseudo-likelihood with fixed unitary clauses",
+              LL_ISE: "log-likelihood with independent soft evidence and weighting of formulas",
+              PLL_ISE: "pseudo-log-likelihood with independent soft evidence",
+              LL_ISEWW: "log-likelihood with independent soft evidence and weighting of worlds",
+              SLL_ISE: "sampled log-likelihood with independent soft evidence and weighting of formulas" # sampled worlds for Z and for gradient
+    }
+    _shortnames = {LL: "LL", PLL: "PLL", BPLL: "BPLL", PLL_fixed: "PLL_fixed", BPLL_fixed: "BPLL_fixed", NPL_fixed: "NPL_fixed", LL_ISE : "LL_ISE", PLL_ISE : "PLL_ISE", LL_ISEWW : "LL_ISEWW", SLL_ISE: "SLL_ISE"}
+    _byName = dict([(x, y) for (y, x) in _names.iteritems()])
