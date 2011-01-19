86abbb31182f90795b3a99027f46d3dea026e6f6
student_jain <student_jain@2a9d9b90-6428-0410-aa80-b0978a1f908e>
2011-01-19 13:04:18 +0000

    * use abs() instead of squared error (because with squared error, the ratio of unprobable/impossible worlds matters for the error and might lead to wrong gradient)
    git-svn-id: svn+ssh://svnradig/probcog/trunk/SRLDB/python@1885 2a9d9b90-6428-0410-aa80-b0978a1f908e

--- python/MLN/methods.py
+++ python/MLN/methods.py
@@ -5,7 +5,7 @@ class InferenceMethods:
     _byName = dict([(x, y) for (y, x) in _names.iteritems()])
     
 class ParameterLearningMeasures:
-    LL, PLL, BPLL, PLL_fixed, BPLL_fixed, NPL_fixed, LL_ISE, PLL_ISE, LL_ISEWW, SE_ISEWW, SLL_ISE, DSLL_ISEWW = range(12)
+    LL, PLL, BPLL, PLL_fixed, BPLL_fixed, NPL_fixed, LL_ISE, PLL_ISE, LL_ISEWW, E_ISEWW, SLL_ISE, DSLL_ISEWW = range(12)
     _names = {LL: "log-likelihood",
               PLL: "pseudo-log-likelihood",
               BPLL: "pseudo-log-likelihood with blocking",
@@ -15,9 +15,9 @@ class ParameterLearningMeasures:
               LL_ISE: "log-likelihood with independent soft evidence and weighting of formulas",
               PLL_ISE: "pseudo-log-likelihood with independent soft evidence",
               LL_ISEWW: "log-likelihood with independent soft evidence and weighting of worlds",
-              SE_ISEWW: "squared error with independent soft evidence and weighting of worlds",
+              E_ISEWW: "error with independent soft evidence and weighting of worlds",
               SLL_ISE: "sampled log-likelihood with independent soft evidence and weighting of formulas", # sampled worlds for Z and for gradient
               DSLL_ISEWW: "double sampled log-likelihood with independent soft evidence and weighting of worlds" # sampled worlds for evidence world in F and for Z
     }
-    _shortnames = {LL: "LL", PLL: "PLL", BPLL: "BPLL", PLL_fixed: "PLL_fixed", BPLL_fixed: "BPLL_fixed", NPL_fixed: "NPL_fixed", LL_ISE : "LL_ISE", PLL_ISE : "PLL_ISE", LL_ISEWW : "LL_ISEWW", SE_ISEWW: "SE_ISEWW", SLL_ISE: "SLL_ISE", DSLL_ISEWW: "DSLL_ISEWW"}
+    _shortnames = {LL: "LL", PLL: "PLL", BPLL: "BPLL", PLL_fixed: "PLL_fixed", BPLL_fixed: "BPLL_fixed", NPL_fixed: "NPL_fixed", LL_ISE : "LL_ISE", PLL_ISE : "PLL_ISE", LL_ISEWW : "LL_ISEWW", E_ISEWW: "E_ISEWW", SLL_ISE: "SLL_ISE", DSLL_ISEWW: "DSLL_ISEWW"}
     _byName = dict([(x, y) for (y, x) in _names.iteritems()])
