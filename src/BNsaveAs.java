333d427b9298704c22241f25560846e59f370e83
jain <jain@2a9d9b90-6428-0410-aa80-b0978a1f908e>
2011-01-19 17:47:49 +0000

    more help output
    git-svn-id: svn+ssh://svnradig/probcog/trunk/SRLDB/src@1893 2a9d9b90-6428-0410-aa80-b0978a1f908e

--- src/BNsaveAs.java
+++ src/BNsaveAs.java
@@ -3,8 +3,9 @@ import edu.tum.cs.bayesnets.core.BeliefNetworkEx;
 
 public class BNsaveAs {
 	public static void main(String[] args) throws Exception {
-		if(args.length != 2) {
-			System.err.println("usage: BNsaveAs <bn file> <bn new file>");
+		if(args.length != 2) {			
+			System.err.println("usage: BNsaveAs <bn file> <new bn filename>");
+			System.err.println("reads and writes any of the supported file formats (format identified by file extensions)");
 			return;
 		}
 		
