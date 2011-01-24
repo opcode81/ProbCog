b5cafa621101d0d1ce294e7e72a59f83dff05a64
jain <jain@2a9d9b90-6428-0410-aa80-b0978a1f908e>
2011-01-24 13:47:11 +0000

    rewritten using visitor pattern
    git-svn-id: svn+ssh://svnradig/probcog/trunk/SRLDB/src@1923 2a9d9b90-6428-0410-aa80-b0978a1f908e

--- src/BNlistCPTs.java
+++ src/BNlistCPTs.java
@@ -2,80 +2,98 @@ import java.io.PrintStream;
 import java.util.HashMap;
 
 import edu.ksu.cis.bnj.ver3.core.BeliefNode;
-import edu.ksu.cis.bnj.ver3.core.CPF;
 import edu.ksu.cis.bnj.ver3.core.Discrete;
 import edu.tum.cs.bayesnets.core.BeliefNetworkEx;
+import edu.tum.cs.bayesnets.core.BeliefNetworkEx.CPTWalker;
 
 
 public class BNlistCPTs {
-
-	protected static HashMap<BeliefNode, Integer> domLengths;
 	
 	/**
 	 * @param args
 	 * @throws Exception 
 	 */
 	public static void main(String[] args) throws Exception {
-		if(args.length != 1) {
-			System.out.println("usage: BNlistCPTs <Bayesian network file>");
-			return;
+		String bnFile = null;
+		boolean onlyNonZero = false;
+		for(String a : args) {
+			if(a.charAt(0) == '-') {
+				if(a.equals("-nz"))
+					onlyNonZero = true;
+			}
+			else
+				bnFile = a;
 		}
 		
-		String bnFile = args[0];
-		//File f = new File(bnFile + ".csv");
-		PrintStream out = System.out; //new PrintStream(f);		
+		if(bnFile == null) {
+			System.out.println("\nprints a network's CPTs to stdout");
+			System.out.println("\n  usage: BNlistCPTs [options] <Bayesian network file>");
+			System.out.println("\n  options: -nz  print only non-zero entries\n");
+			return;
+		}
 		
 		BeliefNetworkEx bn = new BeliefNetworkEx(bnFile);
+		Walker walker = new Walker(bn, onlyNonZero);
 		
-		domLengths = new HashMap<BeliefNode, Integer>();
-		for(BeliefNode node : bn.bn.getNodes()) {
-			int max_length = 0;
-			Discrete dom = (Discrete)node.getDomain();
-			for(int i = 0; i < dom.getOrder(); i++) {
-				max_length = Math.max(max_length, dom.getName(i).length());
-			}
-			domLengths.put(node, max_length);
-		}
-		
-		for(BeliefNode node : bn.bn.getNodes()) {
-			out.println("\n" + node.getName() + ":");
-			
-			CPF cpf = node.getCPF();
-			walkCPT(cpf, 1, new int[cpf.getDomainProduct().length], out);			
+		for(BeliefNode node : bn.bn.getNodes()) {			
+			bn.walkCPT(node, walker, true);			
 		}
 	}
-
-	protected static void walkCPT(CPF cpf, int i, int[] addr, PrintStream out) {
-		BeliefNode[] domProd = cpf.getDomainProduct();		
+	
+	public static class Walker implements CPTWalker {
 		
-		if(i == addr.length) {
-			Discrete dom = (Discrete)domProd[0].getDomain();
-			String condition = null;
-			for(int j = 0; j < dom.getOrder(); j++) {
-				addr[0] = j;
-				double value = cpf.getDouble(addr);
-				if(value != 0) {
-					if(condition == null) {
-						StringBuffer sb = new StringBuffer();
-						for(int k = 1; k < domProd.length; k++) {
-							sb.append(" ");
-							sb.append(domProd[k].getName()).append('=').append(String.format(String.format("%%-%ds", domLengths.get(domProd[k])), domProd[k].getDomain().getName(addr[k])));
-						}
-						condition = sb.toString();
-					}
-					out.printf("%.6f  %s=%s |%s\n", 
-								value, 
-								domProd[0].getName(), 
-								String.format(String.format("%%-%ds", domLengths.get(domProd[0])), domProd[0].getDomain().getName(addr[0])), condition);
+		protected PrintStream out = System.out;
+		protected int domSize, i;
+		protected String condition;
+		protected BeliefNode[] domProd;
+		protected HashMap<BeliefNode, Integer> domLengths;
+		protected boolean onlyNonZero;
+		
+		public Walker(BeliefNetworkEx bn, boolean onlyNonZero) {
+			this.onlyNonZero = onlyNonZero;
+			domLengths = new HashMap<BeliefNode, Integer>();
+			for(BeliefNode node : bn.getNodes()) {
+				int max_length = 0;
+				Discrete dom = (Discrete)node.getDomain();
+				for(int i = 0; i < dom.getOrder(); i++) {
+					max_length = Math.max(max_length, dom.getName(i).length());
 				}
+				domLengths.put(node, max_length);
+			}			
+		}
+
+		@Override
+		public void tellNode(BeliefNode node) {
+			domProd = node.getCPF().getDomainProduct();
+			out.println("\n" + node.getName() + ":");
+		}
+
+		@Override
+		public void tellSize(int childConfigs, int parentConfigs) {
+			i = 0;
+			domSize = childConfigs;			
+		}
+
+		@Override
+		public void tellValue(int[] addr, double value) {			
+			if(i % domSize == 0) {
+				System.out.println("--");
+				StringBuffer sb = new StringBuffer();
+				for(int k = 1; k < domProd.length; k++) {
+					sb.append(" ");
+					sb.append(domProd[k].getName()).append('=').append(String.format(String.format("%%-%ds", domLengths.get(domProd[k])), domProd[k].getDomain().getName(addr[k])));
+				}
+				condition = sb.toString();
 			}
-			return;
-		}
-				
-		Discrete dom = (Discrete)domProd[i].getDomain();
-		for(int j = 0; j < dom.getOrder(); j++) {
-			addr[i] = j;
-			walkCPT(cpf, i+1, addr, out);
-		}
+			
+			if(!onlyNonZero || value != 0) {
+				out.printf("%.6f  %s=%s |%s\n", 
+							value, 
+							domProd[0].getName(), 
+							String.format(String.format("%%-%ds", domLengths.get(domProd[0])), domProd[0].getDomain().getName(addr[0])), condition);
+			}
+			
+			i++;
+		}		
 	}
 }
