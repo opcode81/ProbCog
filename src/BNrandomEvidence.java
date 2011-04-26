9807499bd81b0dba90ccac0354fa2448e032039d
jain <jain@2a9d9b90-6428-0410-aa80-b0978a1f908e>
2011-04-26 21:26:34 +0000

    added program to generate random evidence for a Bayesian network
    git-svn-id: svn+ssh://svnradig/probcog/trunk/SRLDB/src@2108 2a9d9b90-6428-0410-aa80-b0978a1f908e

---
+++ src/BNrandomEvidence.java
@@ -0,0 +1,45 @@
+import java.util.HashSet;
+import java.util.Random;
+
+import edu.ksu.cis.bnj.ver3.core.BeliefNode;
+import edu.ksu.cis.bnj.ver3.core.Domain;
+import edu.tum.cs.bayesnets.core.BeliefNetworkEx;
+
+
+/**
+ * creates random evidence for a Bayesian network
+ * @author jain
+ */
+public class BNrandomEvidence {
+	
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		try {			
+			// read arguments
+			if(args.length != 2) {
+				System.out.println("\n usage: BNrandomEvidence <network> <numEvidence>\n\n");System.exit(1);
+			}			
+			String networkFile = args[0];
+			int numEvidence = Integer.parseInt(args[1]);
+
+			// load model
+			BeliefNetworkEx bn = new BeliefNetworkEx(networkFile);
+			BeliefNode[] nodes = bn.bn.getNodes();
+			HashSet<BeliefNode> handled = new HashSet<BeliefNode>();
+			Random rand = new Random();
+			while(handled.size() < numEvidence) {
+				BeliefNode n = nodes[rand.nextInt(nodes.length)];
+				if(handled.contains(n))
+					continue;
+				handled.add(n);
+				Domain d = n.getDomain();
+				System.out.printf("%s = %s\n", n.toString(), d.getName(rand.nextInt(d.getOrder())));
+			}
+		}
+		catch(Exception e) {
+			e.printStackTrace();
+		}
+	}
+}
