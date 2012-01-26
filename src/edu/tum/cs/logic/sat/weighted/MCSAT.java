35cddbfe5d81ebba96edb19359341184007e4f1f
jain <jain@2a9d9b90-6428-0410-aa80-b0978a1f908e>
2012-01-26 15:17:04 +0000

    added support for soft evidence (MC-SAT-PC)
    git-svn-id: svn+ssh://svnradig/probcog/trunk/SRLDB/src@2998 2a9d9b90-6428-0410-aa80-b0978a1f908e

--- src/edu/tum/cs/logic/sat/weighted/MCSAT.java
+++ src/edu/tum/cs/logic/sat/weighted/MCSAT.java
@@ -6,17 +6,27 @@
  */
 package edu.tum.cs.logic.sat.weighted;
 
+import java.util.Arrays;
+import java.util.HashMap;
 import java.util.Random;
 import java.util.Vector;
 import java.util.Map.Entry;
 
+import salvo.jesus.util.Collections;
+
 import edu.tum.cs.inference.IParameterHandler;
 import edu.tum.cs.inference.ParameterHandler;
+import edu.tum.cs.logic.Atom;
+import edu.tum.cs.logic.Formula;
 import edu.tum.cs.logic.GroundAtom;
+import edu.tum.cs.logic.GroundLiteral;
+import edu.tum.cs.logic.GroundedFormula;
+import edu.tum.cs.logic.Negation;
 import edu.tum.cs.logic.PossibleWorld;
 import edu.tum.cs.logic.WorldVariables;
 import edu.tum.cs.logic.sat.SampleSAT;
 import edu.tum.cs.srl.Database;
+import edu.tum.cs.util.datastruct.Pair;
 
 /**
  * MC-SAT inference algorithm (Poon and Domingos 2006) 
@@ -33,6 +43,18 @@ public class MCSAT implements IParameterHandler {
 	protected int infoInterval = 100;
 	protected ParameterHandler paramHandler;
 	protected SampleSAT sat;
+	protected Vector<SoftEvidence> softEvidence;
+	
+	public class SoftEvidence {
+		public WeightedClause wc;
+		public double p;
+		public double count;
+		public SoftEvidence(WeightedClause wc, double p) {
+			count = 0;
+			this.wc = wc;
+			this.p = p;
+		}
+	}
 	
 	public MCSAT(WeightedClausalKB kb, WorldVariables vars, Database db) throws Exception {		
 		this.kb = kb;
@@ -41,12 +63,13 @@ public class MCSAT implements IParameterHandler {
 		this.rand = new Random();
 		this.dist = new GroundAtomDistribution(vars);
 		this.paramHandler = new ParameterHandler(this);
+		this.softEvidence = new Vector<SoftEvidence>();
 		PossibleWorld state = new PossibleWorld(vars);
 		sat = new SampleSAT(state, vars, db.getEntries());				
 		paramHandler.addSubhandler(sat.getParameterHandler());
 		paramHandler.add("infoInterval", "setInfoInterval");
 		paramHandler.add("verbose", "setVerbose");
-	}
+	}	
 	
 	public WeightedClausalKB getKB() {
 		return kb;
@@ -90,15 +113,28 @@ public class MCSAT implements IParameterHandler {
 		
 		// actual MC-SAT sampling
 		for(int i = 0; i < steps; i++) {
+			
 			M.clear();
+			
 			for(Entry<WeightedFormula, Vector<WeightedClause>> e : kb.getFormulasAndClauses()) {
 				WeightedFormula wf = e.getKey();
 				if(wf.formula.isTrue(sat.getState())){
 					boolean satisfy = wf.isHard || rand.nextDouble() * Math.exp(wf.weight) > 1.0;
 					if(satisfy)
-						M.addAll(e.getValue());
-				}
+						M.addAll(e.getValue());					
+				}				
 			}
+			
+			// soft evidence clauses
+			if(i > 0)
+				for(SoftEvidence se : this.softEvidence) {
+					if(se.wc.isTrue(sat.getState())) {
+						se.count += 1;
+						if(se.count/i < se.p)
+							M.add(se.wc);							
+					}
+				}			
+			
 			if(verbose && (i+1) % infoInterval == 0) {
 				System.out.printf("MC-SAT step %d: %d constraints to be satisfied\n", i+1, M.size());
 				if(debug) {
@@ -108,7 +144,7 @@ public class MCSAT implements IParameterHandler {
 			}
 			sat.initConstraints(M);
 			sat.run();
-			
+	
 			if(false) {
 				sat.getState().print();
 			}
@@ -185,5 +221,12 @@ public class MCSAT implements IParameterHandler {
 	
 	public String getAlgorithmName() {
 		return String.format("%s[%s]", this.getClass().getSimpleName(), sat.getAlgorithmName());
+	}
+	
+	public void addSoftEvidence(Atom f, Double p) throws Exception {
+		Formula ga = f.ground(new HashMap<String,String>(), vars, null);
+		Formula nga = new Negation(ga).toCNF();		
+		this.softEvidence.add(new SoftEvidence(new WeightedClause(ga, 0.0, false), p));
+		this.softEvidence.add(new SoftEvidence(new WeightedClause(nga, 0.0, false), 1.0-p));
 	}
 }
