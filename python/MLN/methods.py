6c9d7f268166606648ac4af78fa198bb4c713559
jain <jain@cs.tum.edu>
2012-02-06 16:04:05 +0000

    cleaned up soft evidence learning methods by adding a general-purpose sampler, some method renaming
    git-svn-id: svn+ssh://svnradig/probcog/trunk/SRLDB/python@3015 2a9d9b90-6428-0410-aa80-b0978a1f908e

--- python/MLN/methods.py
+++ python/MLN/methods.py
@@ -37,17 +37,17 @@ InferenceMethods = Enum(
 ParameterLearningMeasures = Enum(
      {"LL": "log-likelihood",
       "PLL": "pseudo-log-likelihood",
-      "DPLL": "discriminative pseudo-log-likelihood",
+      "DPLL": "[discriminative] pseudo-log-likelihood",
       "BPLL": "pseudo-log-likelihood with blocking",
       "BPLLMemoryEfficient": "pseudo-log-likelihood with blocking, memory-efficient",
       "PLL_fixed": "pseudo-log-likelihood with fixed unitary clauses [deprecated]",
       "BPLL_fixed": "pseudo-log-likelihood with blocking and fixed unitary clauses [deprecated]",
       "NPL_fixed": "negative pseudo-likelihood with fixed unitary clauses [deprecated]",
-      "LL_ISE": "log-likelihood with independent soft evidence and weighting of formulas",
-      "PLL_ISE": "pseudo-log-likelihood with independent soft evidence",
-      "DPLL_ISE": "discriminative pseudo-log-likelihood with independent soft evidence for query atLocation",
-      "LL_ISEWW": "log-likelihood with independent soft evidence and weighting of worlds",
-      "E_ISEWW": "error with independent soft evidence and weighting of worlds",
-      "SLL_ISE": "sampled log-likelihood with independent soft evidence and weighting of formulas", # sampled worlds for Z and for gradient
-      "DSLL_WW": "double sampled log-likelihood with (implicit) weighting of worlds" # sampled worlds for evidence world in F and for Z
+      "LL_ISE": "[soft evidence] log-likelihood with soft features (independent soft evidence)",
+      "PLL_ISE": "[soft evidence] pseudo-log-likelihood with soft features (independent soft evidence)",
+      "DPLL_ISE": "[soft evidence][discriminative] pseudo-log-likelihood with soft features (indep. soft ev.)",
+      "LL_ISEWW": "[soft evidence] log-likelihood with independent soft evidence and weighting of worlds",
+      "E_ISEWW": "[soft evidence] error with independent soft evidence and weighting of worlds",
+      "SLL_ISE": "[soft evidence] sampling-based log-likelihood with soft features (independent soft evidence)", 
+      "SLL_SE": "[soft evidence] sampling-based log-likelihood with soft evidence (MC-SAT-PC sampling)" 
     })
