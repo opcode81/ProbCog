a42e3df5b26eb1bd80a1ec3c594f3e9ac1b67664
jain <jain@cs.tum.edu>
2012-02-10 17:10:55 +0000

    proper implementation of diagonal Newton, some optimizations
    git-svn-id: svn+ssh://svnradig/probcog/trunk/SRLDB/python@3036 2a9d9b90-6428-0410-aa80-b0978a1f908e

--- python/MLN/learning/optimize.py
+++ python/MLN/learning/optimize.py
@@ -1,5 +1,7 @@
 
 import numpy
+import math
+import sys
 
 
 class DirectDescent(object):
@@ -28,80 +30,90 @@ class DirectDescent(object):
 		return self.wt
 
 
-class DiagonalNewton(object):	
+class DiagonalNewton(object):
 	
-	def __init__(self, wt, problem):
+	def __init__(self, wt, problem, gtol=0.01, maxSteps=None):
 		self.problem = problem
 		self.wt = wt
+		self.gtol = gtol
+		self.maxSteps = maxSteps
 	
 	def run(self):
 		p = self.problem
 		wt = numpy.matrix(self.wt).transpose()
+		wtarray = numpy.asarray(wt.transpose())[0]		
 		N = len(wt)
+		l = 0.5				
 		
-		prev_g = numpy.matrix(numpy.zeros(N)).transpose()
-		prev_d = prev_g
-		prev_H = numpy.zeros((N,N))
-		prev_l = 0.5
-		prev_dT = prev_d.transpose()
+		# initial gradient
+		g = numpy.asmatrix(p._grad(wtarray)).transpose()
+		d = numpy.sign(g)
+		dT = d.transpose()		
 		
 		step = 1
-		maxSteps = None
-		
-		while maxSteps is None or step <= maxSteps:
-			wtarray = numpy.asarray(wt.transpose())
+		while self.maxSteps is None or step <= self.maxSteps:			
+			
+			# determine convergence
+			normg = numpy.linalg.norm(g)
+			if normg <= self.gtol:
+				print "\nThreshold reached after %d steps. final gradient: %s (norm: %f <= %f)" % (step, g.transpose(), normg, self.gtol)
+				break
 			
 			H = p._hessian(wtarray)
-			g = numpy.asmatrix(p._grad(wtarray)).transpose()
-			d = numpy.sign(g)
-			dT = d.transpose()
-
-			invH = numpy.zeros((N,N), numpy.float64)
-			illdefined = False
+			
+			# scale gradient with inverse diagonal Hessian
+			sgrad = numpy.asmatrix(numpy.zeros(N)).transpose()
 			for i in xrange(N):
 				v = H[i][i]
 				if v == 0.0: # HACK: if any variance component is zero, set corresponding gradient component to zero
-					v = 1e-6 
-					g[i] = 0.0
-				invH[i][i] = 1.0 / v
+					sgrad[i] = g[i] = 0.0
+				else:				
+					sgrad[i] = g[i] / v
+
+			# compute predicted change in likelihood
+			delta_predict = dT * (g + H * g) / 2.0
+	
+			alpha_numerator = dT * g
+			alpha_q = dT * H * d
 			
-			sgrad = invH * g
-			
-			delta_actual = dT * g
-			delta_predict = prev_dT * (prev_g + prev_H * prev_g) / 2.0
-			frac = delta_actual / delta_predict
-			if frac > 0.75 : l = prev_l / 2.0
-			elif frac < 0.25: l = prev_l * 4.0
-			else: l = prev_l
-			
-			alpha = dT * g
-			q = dT * H * d
-			alpha /=  q + l * dT * d
-			alpha = float(alpha)
-			
-			print
-			print "H:\n%s" % H
-			print "grad: %s" % g.transpose()
-			print "sgrad: %s" % sgrad.transpose()
-			print "delta_p: %f" % delta_predict
-			print "delta_a: %f" % delta_actual
-			print "lambda: %f" % l
-			print "alpha: %f" % alpha			
-			print "old wt: %s" % wt.transpose()
-			wt = wt + alpha * sgrad
-			print "new wt: %s" % wt.transpose()
-			
-			breakAlpha = 0.001
-			if alpha < breakAlpha:
-				print "alpha dropped below %f, stopping." % breakAlpha
-				break
-		
-			prev_g = g
-			prev_d = d
-			prev_H = H
-			prev_l = l
-			prev_dT = dT
-			
+			while True:
+				# determine step coefficient
+				alpha = numpy.float(alpha_numerator / (alpha_q + l * dT * d))
+	
+				# make the step
+				wt_old = numpy.matrix(wt)
+				wt += alpha * sgrad
+				wtarray = numpy.asarray(wt.transpose())[0]				
+				
+				# compute new gradient
+				g = numpy.asmatrix(p._grad(wtarray)).transpose()
+				d = numpy.sign(g)
+				dT = d.transpose()
+				
+				# estimate actual likelihood change
+				delta_actual = dT * g
+
+				# update lambda
+				frac = delta_actual / delta_predict
+				if frac > 0.75 : l /= 2.0 # (lambda may become zero due to precision loss; worry?)
+				elif frac < 0.25: l *= 4.0
+				
+				if delta_actual >= 0: # accept move					
+					print
+					print "H:\n%s" % H
+					print "|g|: %f" % normg
+					print "sgrad: %s" % sgrad.transpose()
+					print "delta_a: %f" % delta_actual
+					print "delta_p: %f" % delta_predict					
+					print "lambda: %f" % l
+					print "alpha: %f" % alpha			
+					print "old wt: %s" % wt_old.transpose()
+					print "new wt: %s" % wt.transpose()
+					break
+				else: # reject move
+					print "delta_a=%f, adjusted lambda to %f" % (delta_actual, l)
+					wt = wt_old
+
 			step += 1
 		
-		return numpy.asarray(wt.transpose())
+		return numpy.asarray(wt.transpose())[0]
