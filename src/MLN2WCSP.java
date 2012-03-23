177b5400bf0a60ce7ce566e56fc3c81a8a220f60
jain <jain@cs.tum.edu>
2012-03-23 18:53:54 +0000

    heavy refactoring of the WCSP converter
    git-svn-id: svn+ssh://svnradig/probcog/trunk/SRLDB/src@3084 2a9d9b90-6428-0410-aa80-b0978a1f908e

--- src/MLN2WCSP.java
+++ src/MLN2WCSP.java
@@ -3,9 +3,6 @@ import edu.tum.cs.wcsp.WCSPConverter;
 
 /*
  * Created on Aug 3, 2009
- *
- * TODO To change the template for this generated file go to
- * Window - Preferences - Java - Code Style - Code Templates
  */
 
 public class MLN2WCSP {
@@ -16,14 +13,11 @@ public class MLN2WCSP {
 	 */
 	public static void main(String[] args) throws Exception {
 		if(args.length < 3) {
-			System.out.println("usage: MLN2WCSP <MLN file> <evidence database file> <WCSP output file> [scenario output file]");
+			System.out.println("usage: MLN2WCSP <MLN file> <evidence database file> <WCSP output file>");
 			return;			
 		}
 		
 		WCSPConverter converter = new WCSPConverter(args[0], args[1]);
-		String scenarioFile = null;
-		if(args.length >= 4)
-			scenarioFile = args[3];
-		converter.run(args[2], scenarioFile);		
+		converter.run(args[2]);		
 	}
 }
