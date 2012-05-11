7d624243a58a023e768f96b59e89e347b1960f4c
jain <jain@cs.tum.edu>
2012-05-11 16:47:41 +0000

    added reading of .wcsp files
    git-svn-id: svn+ssh://svnradig/probcog/trunk/SRLDB/src@3143 2a9d9b90-6428-0410-aa80-b0978a1f908e

--- src/edu/tum/cs/wcsp/WCSP.java
+++ src/edu/tum/cs/wcsp/WCSP.java
@@ -6,6 +6,10 @@
  */
 package edu.tum.cs.wcsp;
 
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileReader;
+import java.io.IOException;
 import java.util.Iterator;
 import java.util.Vector;
 
@@ -41,5 +45,39 @@ public class WCSP implements Iterable<Constraint> {
 	@Override
 	public Iterator<Constraint> iterator() {
 		return constraints.iterator();
+	}
+	
+	public static WCSP fromFile(File f) throws IOException {
+		BufferedReader br = new BufferedReader(new FileReader(f));
+		String l = br.readLine();
+		String[] elems = l.split(" ");
+		int numVars = Integer.parseInt(elems[1]);
+		int numConstraints = Integer.parseInt(elems[3]);
+		long top = Integer.parseInt(elems[4]);
+		elems = br.readLine().split(" ");
+		int[] domSizes = new int[numVars];
+		for(int i = 0; i < numVars; i++)
+			domSizes[i] = Integer.parseInt(elems[i]);
+		WCSP wcsp = new WCSP(numVars, domSizes, top);
+		for(int i = 0; i < numConstraints; i++) {
+			elems = br.readLine().split(" ");
+			int arity = Integer.parseInt(elems[0]);
+			int[] varIndices = new int[arity];
+			for(int j = 1; j <= arity; j++)
+				varIndices[j-1] = Integer.parseInt(elems[j]);
+			long defaultCost = Long.parseLong(elems[1+arity]);
+			int numTuples = Integer.parseInt(elems[1+arity+1]);
+			Constraint c = new Constraint(defaultCost, varIndices, numTuples);
+			for(int j = 0; j < numTuples; j++) {
+				elems = br.readLine().split(" ");
+				int[] assignment = new int[arity];
+				for(int k = 0; k < arity; k++)
+					assignment[k] = Integer.parseInt(elems[k]);
+				long cost = Long.parseLong(elems[arity]);
+				c.addTuple(assignment, cost);
+			}
+			wcsp.addConstraint(c);
+		}
+		return wcsp;
 	}
 }
