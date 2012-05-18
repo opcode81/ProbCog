1b49d635fd9188cc3a34e4d519643144a0198032
jain <jain@cs.tum.edu>
2012-05-18 11:14:35 +0000

    minor
    git-svn-id: svn+ssh://svnradig/probcog/trunk/SRLDB/src@3183 2a9d9b90-6428-0410-aa80-b0978a1f908e

--- src/probcog/logic/sat/weighted/WeightedFormula.java
+++ src/probcog/logic/sat/weighted/WeightedFormula.java
@@ -1,8 +1,5 @@
 /*
  * Created on Aug 10, 2009
- *
- * TODO To change the template for this generated file go to
- * Window - Preferences - Java - Code Style - Code Templates
  */
 package probcog.logic.sat.weighted;
 
