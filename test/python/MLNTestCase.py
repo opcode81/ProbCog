6f58aaacf1cba166ccb38476a21edd1268f21518
Dominik Jain <djain@gmx.net>
2013-02-24 19:57:03 +0100

    more tests

--- test/python/MLNTestCase.py
+++ test/python/MLNTestCase.py
@@ -8,19 +8,23 @@ from pprint import pprint
 class MLNTestCase(unittest.TestCase):
     
     def setUp(self):
-        self.modelsDir = os.path.join("..", "models")
-        self.smokingDir = os.path.join(self.modelsDir, "smokers")
-        self.smokingDB = os.path.join(self.smokingDir, "smoking-train.db")
-        self.studentDir = os.path.join(self.modelsDir, "student_course")
-        self.studentDB = os.path.join(self.studentDir, "1each.db")
-        self.probConstrDir = os.path.join(self.modelsDir, "probConstraints")
-        self.posteriorCDir = os.path.join(self.probConstrDir, "posterior")
-        self.priorCDir = os.path.join(self.probConstrDir, "prior")
+        join = os.path.join
+        self.modelsDir = join("..", "models")
+        self.simpleLearnDir = join(self.modelsDir, "simpleLearning")
+        self.simpleLearnDB = join(self.simpleLearnDir, "train.db")
+        self.smokingDir = join(self.modelsDir, "smokers")
+        self.smokingDB = join(self.smokingDir, "smoking-train.db")
+        self.studentDir = join(self.modelsDir, "student_course")
+        self.studentDB = join(self.studentDir, "1each.db")
+        self.probConstrDir = join(self.modelsDir, "probConstraints")
+        self.posteriorCDir = join(self.probConstrDir, "posterior")
+        self.priorCDir = join(self.probConstrDir, "prior")
     
-    def assertApproxEqual(self, a, b, delta = 1e-7):
-        self.assertTrue(abs(a-b) <= delta)
+    def assertApproxEqual(self, a, b, delta = 1e-5):
+        self.assertTrue(abs(a-b) < delta)
+            #raise AssertionError("%f !~ %f [max. dev. %f]" % (a, b, delta))
     
-    def assertApproxListEqual(self, a, b, delta = 1e-7):
+    def assertApproxListEqual(self, a, b, delta = 1e-5):
         self.assertEqual(len(a), len(b))
         for i in xrange(len(a)):
             self.assertApproxEqual(a[i], b[i], delta)
@@ -28,35 +32,57 @@ class MLNTestCase(unittest.TestCase):
     def getSmokersModel(self):
         return MLN(os.path.join(self.smokingDir, "smoking.mln"))
     
-    def test_learnBPLL(self):        
+    def getSimpleLearningModel(self):
+        return MLN(os.path.join(self.simpleLearnDir, "simple.mln"))
+    
+    def test_learnLL(self):
+        mln = self.getSimpleLearningModel()
+        mln.learnWeights([self.simpleLearnDB], ParameterLearningMeasures.LL, optimizer="bfgs")
+        weights = mln.getWeights()
+        correctWeights = [12.134464056525509, -4.0448208849872236, -8.0896429592875982, 4.0448220742997858]
+        self.assertApproxListEqual(weights, correctWeights)
+
+    def test_learnPLL(self):
+        mln = self.getSimpleLearningModel()
+        mln.learnWeights([self.simpleLearnDB], ParameterLearningMeasures.PLL, optimizer="bfgs")
+        weights = mln.getWeights()
+        correctWeights = [16.946119791748266, -5.6487239615488454, -11.297429358877265, 5.6487053973271903]
+        self.assertApproxListEqual(weights, correctWeights)
+    
+    def test_learnBPLL(self):
+        mln = self.getSimpleLearningModel()
+        mln.learnWeights([self.simpleLearnDB], ParameterLearningMeasures.BPLL, optimizer="bfgs")
+        weights = mln.getWeights()
+        correctWeights = [16.946119791748266, -5.6487239615488454, -11.297429358877265, 5.6487053973271903]
+        self.assertApproxListEqual(weights, correctWeights)
+
+    def test_learnBPLL_Smoking(self):        
         mln = self.getSmokersModel()
         mln.learnWeights([self.smokingDB], ParameterLearningMeasures.BPLL, optimizer="bfgs")
+        weights = mln.getWeights()
         correctWeights = [1.1267686091064575, 1.5777760206174236]
-        for i, f in enumerate(mln.formulas):
-            self.assertAlmostEqual(correctWeights[i], f.weight)
+        self.assertApproxListEqual(weights, correctWeights)
     
-    def test_learnPLL(self):        
+    def __test_learnPLL_Smoking(self):        
         mln = self.getSmokersModel()
         mln.learnWeights([self.smokingDB], ParameterLearningMeasures.PLL, optimizer="bfgs")
         correctWeights = [0.664496216335284, 1.8004196461831026]
-        for i, f in enumerate(mln.formulas):
-            self.assertAlmostEqual(correctWeights[i], f.weight)
+        weights = mln.getWeights()
+        self.assertApproxListEqual(weights, correctWeights)
     
     def test_groundAMLN(self):
         mln = MLN(os.path.join(self.studentDir, "student_course2.a.mln"))
         mrf = mln.groundMRF(self.studentDB)
         weights = map(lambda p: float(p[0]), mrf.getGroundFormulas())
         correctWeights = [1.5604290525533999, 3.6511741317144004, -0.7968133588156, 7.0145072690164003, 0.151252120947, -1.6360797755282002, -1.5131502151878, -8.4313192249819995, -0.38454063647300002, -1.16283955905, 1.3519705598595, -2.8760315103522003]
-        for i, f in enumerate(mln.formulas):
-            self.assertAlmostEqual(correctWeights[i], f.weight)
+        self.assertApproxListEqual(weights, correctWeights)
     
     def test_groundWithVars(self):
         mln = MLN(os.path.join(self.studentDir, "student_course2_simLearned.mln"))
         mrf = mln.groundMRF(self.studentDB)
         weights = map(lambda p: float(p[0]), mrf.getGroundFormulas())
         correctWeights = [-0.40546510810816444, -1.0986122886681098, -0.22314355131420957, -1.6094379124341012, -7.3508372506616082, -0.0006422607799453446, -1.0986122886681098, -0.40546510810816427, -6.1468644463356723, -0.0021424753776469402, 27.350837250661606, 27.350837250661606]
-        for i, f in enumerate(mln.formulas):
-            self.assertAlmostEqual(correctWeights[i], f.weight)
+        self.assertApproxListEqual(weights, correctWeights)
     
     def test_softEvidenceIPFPM(self):
         mln = MLN(os.path.join(self.posteriorCDir, "simple.mln"))
@@ -71,6 +97,8 @@ class MLNTestCase(unittest.TestCase):
         results = map(float, (mrf.inferMCSAT("attr")))
         correctResults = [0.6666666666666666, 0.10]
         self.assertApproxListEqual(results, correctResults, 0.04)
+    
+    # TODO: multiple database learning, fixed weights
         
 if __name__ == '__main__':
     unittest.main()
