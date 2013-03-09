e99b952b91eb58307d3053705b2660c62b7a37aa
jain <djain@gmx.net>
2013-03-09 02:11:10 +0100

    made default grounding runnable

--- python/MLN/grounding/AbstractGrounding.py
+++ python/MLN/grounding/AbstractGrounding.py
@@ -21,8 +21,6 @@
 # TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE
 # SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
 
-import FOL
-import re
 
 class AbstractGroundingFactory(object):
     
@@ -43,93 +41,3 @@ class AbstractGroundingFactory(object):
         self.mrf.softEvidence = self.db.softEvidence
         self._createGroundFormulas(verbose)
         return self.mrf
-        
-
-class DefaultGroundingFactory(AbstractGroundingFactory):
-    '''
-    Implementation of the default grounding algorithm, which
-    creates ALL ground atoms and ALL ground formulas.
-    '''
-    
-    def _createGroundAtoms(self, verbose=False):
-        # create ground atoms
-        atoms = []
-        for predName, domNames in self.mln.predicates.iteritems():
-            self._groundAtoms([], predName, domNames, verbose)
-
-    def _groundAtoms(self, cur, predName, domNames, verbose=False):
-        # if there are no more parameters to ground, we're done
-        # and we cann add the ground atom to the MRF
-        mrf = self.mrf
-        assert len(mrf.gndFormulas) == 0
-        if domNames == []:
-            atom = FOL.GroundAtom(predName, cur)
-            mrf.addGroundAtom(atom)
-            return
-
-        # create ground atoms for each way of grounding the first of the 
-        # remaining variables whose domains are given in domNames
-        dom = mrf.domains.get(domNames[0])
-        if dom is None or len(dom) == 0:
-            raise Exception("Domain '%s' is empty!" % domNames[0])
-        for value in dom:
-            self._groundAtoms(cur + [value], predName, domNames[1:])
-        
-    def _createGroundFormulas(self, verbose=False):
-        mrf = self.mrf
-        assert len(mrf.gndAtoms) > 0
-        
-        # generate all groundings
-        if verbose: 
-            print "Grounding formulas..."
-        for idxFormula, formula in enumerate(mrf.formulas):
-            if verbose: 
-                print "  %s" % strFormula(formula)
-            for gndFormula, referencedGndAtoms in formula.iterGroundings(mrf, mrf.simplify):
-                gndFormula.isHard = formula.isHard
-                gndFormula.weight = formula.weight
-                if isinstance(gndFormula, FOL.TrueFalse):
-                    continue
-                mrf._addGroundFormula(gndFormula, idxFormula, referencedGndAtoms)
-
-        # materialize all formula weights
-        max_weight = 0
-        for f in mrf.formulas:
-            if f.weight is not None:
-                if hasattr(f, "complexWeight"): # TODO check if complexWeight is ever used anywhere (old AMLN learning?)
-                    f.weight = f.complexWeight
-                w = str(f.weight)
-                f.complexWeight = w
-                while "$" in w:
-                    try:
-                        w, numReplacements = re.subn(r'\$\w+', self._substVar, w)
-                    except:
-                        sys.stderr.write("Error substituting variable references in '%s'\n" % w)
-                        raise
-                    if numReplacements == 0:
-                        raise Exception("Undefined variable(s) referenced in '%s'" % w)
-                w = re.sub(r'domSize\((.*?)\)', r'self.domSize("\1")', w)
-                try:
-                    f.weight = eval(w)
-                except:
-                    sys.stderr.write("Evaluation error while trying to compute '%s'\n" % w)
-                    raise
-                max_weight = max(abs(f.weight), max_weight)
-
-        # set weights of hard formulas
-        hard_weight = 20 + max_weight
-        if verbose: 
-            print "setting %d hard weights to %f" % (len(mrf.hard_formulas), hard_weight)
-        for f in mrf.hard_formulas:
-            if verbose: 
-                print "  ", strFormula(f)
-            f.weight = hard_weight
-        
-        self.mln.gndFormulas = mrf.gndFormulas
-        self.mln.gndAtomOccurrencesInGFs = mrf.gndAtomOccurrencesInGFs
-
-        
-        
-        
-        
-        
