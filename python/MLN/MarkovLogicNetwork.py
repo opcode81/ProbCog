abd826985978bf0986a74f9b4d6fd730bab68c1d
jain <djain@gmx.net>
2013-03-09 02:10:31 +0100

    reverted restriction to float weights

--- python/MLN/MarkovLogicNetwork.py
+++ python/MLN/MarkovLogicNetwork.py
@@ -286,7 +286,7 @@ class MLN(object):
                             formula = line[:-1]
                         else: # with weight
                             spacepos = line.find(' ')
-                            weight = float(line[:spacepos])
+                            weight = line[:spacepos]
                             formula = line[spacepos:].strip()
                         try:
                             formula = FOL.parseFormula(formula)
